a22f245caf310efb0e755256a166f38bf474e72e
orcakill <orcakill@dingtalk.com>
2020-07-24 14:18:07 +0800

    leetcode

---
+++ src/main/java/leetcode/leet1025.java
@@ -0,0 +1,26 @@
+package leetcode;
+
+public class leet1025 {
+    static class Solution {
+        public static boolean divisorGame(int N) {
+            boolean als = false;
+            for (int i = 1; i < N; i++) {
+                int n=N;
+                for (int j=1;j<n;j++){
+                if (N % i == 0) {
+                    n = N - i;
+                }
+                    if (i % 2 != 0 && N - i == 1) {
+                        als = true;
+                        break;
+                    } else if (i % 2 == 0 && N - i == 1) {
+                        als = false;
+                        break;
+                    }
+                }
+            }
+
+            return als;
+        }
+    }
+}
