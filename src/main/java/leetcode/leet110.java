86ec869a8d2a97f242595c9d854d1885a5551463
orcakill <orcakill@dingtalk.com>
2020-08-18 11:05:44 +0800

    默认的

---
+++ src/main/java/leetcode/leet110.java
@@ -0,0 +1,36 @@
+package leetcode;
+
+import leetclass.TreeNode;
+
+//完全不懂
+public class leet110 {
+    public static void main(String[] args) {
+        TreeNode t=new TreeNode();
+        System.out.println(leet110.Solution.isBalanced(t));
+    }
+
+
+
+    static class Solution {
+        public static boolean isBalanced(TreeNode root) {
+                return height(root) >= 0;
+            }
+
+            public static int height(TreeNode root) {
+                if (root == null) {
+                    return 0;
+                }
+                int leftHeight = height(root.left);
+                int rightHeight = height(root.right);
+                if (leftHeight == -1 || rightHeight == -1 || Math.abs(leftHeight - rightHeight) > 1) {
+                    return -1;
+                } else {
+                    return Math.max(leftHeight, rightHeight) + 1;
+                }
+            }
+
+        }
+
+
+
+}
