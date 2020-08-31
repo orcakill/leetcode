5858dc87184160514641a2e03f0b00122970dea6
orcakill <orcakill@dingtalk.com>
2020-08-31 19:08:08 +0800

    8月28日 leet657更新

---
+++ src/main/java/leetcode/leet841.java
@@ -0,0 +1,24 @@
+package leetcode;
+
+import java.util.List;
+import util.array_list;
+
+public class leet841 {
+    public static void main(String[] args) {
+        int a[][]={{1},{2},{3},{}};
+        int b[][]={{1,3},{3,0,1},{2},{0}};
+        int  c[]={1,2,3};
+        List<int[]> list1=array_list.array_list_one(c);
+
+
+
+    }
+
+
+
+    public static boolean canVisitAllRooms(List<List<Integer>> rooms) {
+
+        return true;
+    }
+
+}
---
+++ src/main/java/util/two_array_list.java
@@ -0,0 +1,11 @@
+package util;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class two_array_list {
+    public static List<int[]> array_list_two(int[][] arr1){
+        List<int[]> tbList = Arrays.asList(arr1);
+        return  tbList;
+    }
+}
