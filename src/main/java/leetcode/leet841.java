b6038347a625a40bf78ba2795754ef6b0025d252
orcakill <orcakill@dingtalk.com>
2020-08-31 23:01:35 +0800

    2020/8/31

--- src/main/java/leetcode/leet841.java
+++ src/main/java/leetcode/leet841.java
@@ -1,15 +1,22 @@
 package leetcode;
 
+import java.util.ArrayList;
 import java.util.List;
 import util.array_list;
+import  util.two_array_list;
 
 public class leet841 {
     public static void main(String[] args) {
         int a[][]={{1},{2},{3},{}};
         int b[][]={{1,3},{3,0,1},{2},{0}};
         int  c[]={1,2,3};
-        List<int[]> list1=array_list.array_list_one(c);
 
+        int d[][]={{1,3},{3,0,1},{2},{0}};
+        int e[][]={{4},{3},{},{2,5,7},{1},{},{8,9},{},{},{6}};
+                 // 0   1   2   3      4   5   6    7  8   9
+        int f[][]={{6,7,8},{5,4,9},{},{8},{4},{},{1,9,2,3},{7},{6,5},{2,3,1}};
+        List<List<Integer>> list1=two_array_list.array_list_two(e);
+        System.out.println(canVisitAllRooms(list1));
 
 
     }
@@ -17,7 +24,23 @@ public class leet841 {
 
 
     public static boolean canVisitAllRooms(List<List<Integer>> rooms) {
+        List<Integer> list0=new ArrayList<>();
+        for(int i=0;i<rooms.size();i++){
+            list0.add(0);
+        }
+        list0.set(0,1);
+        return  digui(rooms,list0);
+    }
 
+    public static   boolean digui(List<List<Integer>> rooms,List<Integer> list0){
+       for(int i=0;i<list0.size();i++) {
+              if(1 == list0.get(i)){
+                  for (int j = 0; j < rooms.get(i).size(); j++) {
+                      list0.set(rooms.get(i).get(j), 1);
+                  }
+              }
+
+       }
         return true;
     }
 
--- src/main/java/util/two_array_list.java
+++ src/main/java/util/two_array_list.java
@@ -1,11 +1,20 @@
 package util;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
 public class two_array_list {
-    public static List<int[]> array_list_two(int[][] arr1){
-        List<int[]> tbList1 = Arrays.asList(arr1);
-        return  tbList1;
+    public static  List<List<Integer>> array_list_two(int[][] arr1){
+        List<List<Integer>> llist1=new ArrayList<List<Integer>>();
+        for(int i=0;i<arr1.length;i++){
+            List<Integer> list1=new ArrayList<>();
+            int arr2[]=arr1[i];
+            for(int j=0;j<arr2.length;j++){
+                list1.add(arr2[j]);
+            }
+            llist1.add(list1);
+        }
+        return  llist1;
     }
 }
