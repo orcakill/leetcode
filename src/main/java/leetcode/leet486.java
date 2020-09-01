044012faa0c7e379f6b803332b25aa6056680d02
orcakill <orcakill@dingtalk.com>
2020-09-01 18:32:14 +0800

    2020/9/01 更新

---
+++ src/main/java/leetcode/leet486.java
@@ -0,0 +1,13 @@
+package leetcode;
+
+public class leet486 {
+    public static void main(String[] args) {
+        int a[]= {1, 5, 2};  //flase
+        System.out.println(PredictTheWinner(a));
+    }
+
+    public static boolean PredictTheWinner(int[] nums) {
+
+     return true;
+    }
+}
