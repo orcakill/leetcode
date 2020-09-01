3aada34da411457220e51d67b7c54d43778f8930
orcakill <orcakill@dingtalk.com>
2020-09-01 10:44:01 +0800

    2020/9/01 更新

--- src/main/java/leetcode/leet841.java
+++ src/main/java/leetcode/leet841.java
@@ -1,47 +1,52 @@
 package leetcode;
 
 import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.LinkedList;
 import java.util.List;
+
 import util.array_list;
-import  util.two_array_list;
+import util.two_array_list;
 
 public class leet841 {
     public static void main(String[] args) {
-        int a[][]={{1},{2},{3},{}};
-        int b[][]={{1,3},{3,0,1},{2},{0}};
-        int  c[]={1,2,3};
-
-        int d[][]={{1,3},{3,0,1},{2},{0}};
-        int e[][]={{4},{3},{},{2,5,7},{1},{},{8,9},{},{},{6}};
-                 // 0   1   2   3      4   5   6    7  8   9
-        int f[][]={{6,7,8},{5,4,9},{},{8},{4},{},{1,9,2,3},{7},{6,5},{2,3,1}};
-        List<List<Integer>> list1=two_array_list.array_list_two(e);
+        int a[][] = {{1}, {2}, {3}, {}};
+        int b[][] = {{1, 3}, {3, 0, 1}, {2}, {0}};
+        int c[] = {1, 2, 3};
+        int d[][] = {{1, 3}, {3, 0, 1}, {2}, {0}};
+        int e[][] = {{4}, {3}, {}, {2, 5, 7}, {1}, {}, {8, 9}, {}, {}, {6}}; /* 0   1   2   3      4   5   6    7  8   9*/
+        int f[][] = {{6, 7, 8}, {5, 4, 9}, {}, {8}, {4}, {}, {1, 9, 2, 3}, {7}, {6, 5}, {2, 3, 1}}; /* 0      1     2  3   4  5   6         7    8      9*/
+        List<List<Integer>> list1 = two_array_list.array_list_two(b);
         System.out.println(canVisitAllRooms(list1));
-
-
     }
-
-
 
     public static boolean canVisitAllRooms(List<List<Integer>> rooms) {
-        List<Integer> list0=new ArrayList<>();
-        for(int i=0;i<rooms.size();i++){
-            list0.add(0);
+        if(rooms.size()==1) {
+            return true;
         }
-        list0.set(0,1);
-        return  digui(rooms,list0);
+        HashSet<Integer> set = new HashSet<>();
+        LinkedList<Integer> list = new LinkedList<>();
+        int length = rooms.size();
+        list.add(0);
+        set.add(0);
+        while(!list.isEmpty()) {
+            int room_id = list.poll();
+            List<Integer> roomId_list = rooms.get(room_id);
+            for(int i:roomId_list) {
+                if(set.contains(i)) {
+                    continue;
+                }
+                set.add(i);
+                list.add(i);
+            }
+        }
+        int flag = 0;
+        for(int j=0;j<length;j++) {
+            if(!set.contains(j)) {
+                flag = 1;
+                break;
+            }
+        }
+        return flag==0;
     }
-
-    public static   boolean digui(List<List<Integer>> rooms,List<Integer> list0){
-       for(int i=0;i<list0.size();i++) {
-              if(1 == list0.get(i)){
-                  for (int j = 0; j < rooms.get(i).size(); j++) {
-                      list0.set(rooms.get(i).get(j), 1);
-                  }
-              }
-
-       }
-        return true;
-    }
-
 }
