20164ec1b75dc599a07b79866e9197081d735285
orcakill <orcakill@dingtalk.com>
2020-09-02 18:43:21 +0800

    2020/9/02 更新

--- src/main/java/leetcode/leet486.java
+++ src/main/java/leetcode/leet486.java
@@ -7,7 +7,19 @@ public class leet486 {
     }
 
     public static boolean PredictTheWinner(int[] nums) {
-
-     return true;
+        return total(nums, 0, nums.length - 1, 1) >= 0;
     }
+
+    public static int total(int[] nums, int start, int end, int turn) {
+        if (start == end) {
+            return nums[start] * turn;
+        }
+        int scoreStart = nums[start] * turn + total(nums, start + 1, end, -turn);
+        int scoreEnd = nums[end] * turn + total(nums, start, end - 1, -turn);
+        int i1=scoreStart * turn;
+        int i2=scoreStart * turn;
+        return Math.max(scoreStart * turn, scoreEnd * turn) * turn;
+    }
+
+
 }
