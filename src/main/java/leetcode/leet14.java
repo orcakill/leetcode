2188423737409c90cdc82b8b68bb8be30cbb6400
orcakill <orcakill@dingtalk.com>
2020-09-24 15:13:55 +0800

    2020/9/24 更新

---
+++ src/main/java/leetcode/leet14.java
@@ -0,0 +1,40 @@
+package leetcode;
+
+import leetclass.ListNode;
+
+public class leet14 {
+    public static void main(String[] args) {
+         String[] a={"flower","flow","flight"};
+         String[] b={"dog","racecar","car"};
+        String[] c={""};
+         System.out.println(longestCommonPrefix(c));
+
+    }
+
+    public static String longestCommonPrefix(String[] strs) {
+        if (strs == null || strs.length == 0) {
+            return "";
+        }
+        String prefix = strs[0];
+        int count = strs.length;
+        for (int i = 1; i < count; i++) {
+            prefix = longestCommonPrefix(prefix, strs[i]);
+            if (prefix.length() == 0) {
+                break;
+            }
+        }
+        return prefix;
+    }
+
+    public static String longestCommonPrefix(String str1, String str2) {
+        int length = Math.min(str1.length(), str2.length());
+        int index = 0;
+        while (index < length && str1.charAt(index) == str2.charAt(index)) {
+            index++;
+        }
+        return str1.substring(0, index);
+
+
+    }
+
+}
