a669fe216431eea7ee97315416dc356dc96d4e6b
orcakill <orcakill@dingtalk.com>
2020-10-29 11:08:13 +0800

    2020/10/29 更新leet28

--- src/main/java/leetcode/leet28.java
+++ src/main/java/leetcode/leet28.java
@@ -4,7 +4,7 @@ public class leet28 {
     public static void main(String[] args) {
         String haystack0="hello";
         String needle0="ll";
-
+          /*测试用例2*/
         String haystack1="aaa";
         String needle1="a";
         System.out.println(strStr(haystack1,needle1));
