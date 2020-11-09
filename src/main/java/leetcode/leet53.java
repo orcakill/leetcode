ffab5cf5094943a5fe286b13fc7872c985aead58
orcakill <orcakill@dingtalk.com>
2020-11-09 14:52:30 +0800

    2020/11/9 更新leet53

---
+++ src/main/java/leetcode/leet53.java
@@ -0,0 +1,40 @@
+package leetcode;
+
+import java.util.ArrayList;
+
+
+public class leet53 {
+    public static void main(String[] args) {
+        int  a[]={-2,1,-3,4,-1,2,1,-5,4};
+        int  b[]={1};
+        int  c[]={-2,1};
+        int  d[]={-2,-1};
+        System.out.println(maxSubArray(c));
+    }
+
+    public static int maxSubArray(int[] nums) {
+        ArrayList<Integer> arrayList=new ArrayList<>();
+        int  max=nums[0];
+        if(nums.length==1){
+            return nums[0];
+        }
+        for(int i=0;i<nums.length;i++){
+                arrayList.add(nums[i]);
+        }
+        for(int i=0;i<arrayList.size();i++){
+            int  x=arrayList.get(i);
+            if(x>max){
+                max=x;
+            }
+            for(int j=i+1;j<arrayList.size();j++){
+                x=x+arrayList.get(j);
+                if(x>max){
+                    max=x;
+                }
+            }
+        }
+        return  max;
+
+    }
+
+}
