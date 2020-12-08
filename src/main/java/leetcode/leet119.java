07ce526d47e013676d126ca94933b4cd6985d021
orcakill <orcakill@dingtalk.com>
2020-12-08 22:40:34 +0800

    2020/12/08 更新 leet119

--- src/main/java/leetcode/leet119.java
+++ src/main/java/leetcode/leet119.java
@@ -5,13 +5,25 @@ import java.util.List;
 
 public class leet119 {
     public static void main(String[] args) {
+        System.out.println(getRow(1));
         System.out.println(getRow(2));
+        System.out.println(getRow(3));
+        System.out.println(getRow(4));
+        System.out.println(getRow(5));
+
     }
 
     public static List<Integer> getRow(int rowIndex) {
-        List<Integer> list=new ArrayList<>();
-        for(int i=0;i<rowIndex;i++){
-            list.add(i+1);
+        ArrayList<Integer> list=new ArrayList<>();
+        list.add(1);
+        for(int i=1;i<rowIndex+1;i++){
+            list.add(0);
+        }
+        for (int i = 1; i < rowIndex+1; i++){
+            for(int j=i; j > 0; j--) {
+                Integer x=list.get(j)+list.get(j-1);
+                list.set(j,x) ;
+            }
         }
         return  list;
     }
