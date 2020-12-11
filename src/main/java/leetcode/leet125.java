abcf99fcb37a0992f596f42aa0b65073dcc8ddb6
orcakill <orcakill@dingtalk.com>
2020-12-11 17:08:07 +0800

    2020/12/11 更新 leet122

---
+++ src/main/java/leetcode/leet125.java
@@ -0,0 +1,29 @@
+package leetcode;
+
+public class leet125 {
+    public static void main(String[] args) {
+        String a="A man, a plan, a canal: Panama";
+        String b="`l;`` 1o1 ??;l`";
+//        System.out.println(isPalindrome(a));
+        System.out.println(isPalindrome(b));
+
+    }
+    public static boolean isPalindrome(String s) {
+        if(s.length()==0){
+            return true;
+        }
+        String s0=s.replaceAll("[\\pP\\p{Punct}]","");
+        String s1=new String();
+        for(int i=0;i<s0.length();i++){
+            String ss=s0.substring(i,i+1).toLowerCase();
+            if(!ss.equals(" ")&&!ss.equals("''")){
+               s1+=ss;
+            }
+        }
+        StringBuffer s2=new StringBuffer(s1);
+        StringBuffer s3=s2.reverse();
+        String       s4=new String(s3);
+
+        return s1.equals(s4);
+    }
+}
