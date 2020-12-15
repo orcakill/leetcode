774c5ff0e24d5086f23b2af7ed946b47dbe43101
orcakill <orcakill@dingtalk.com>
2020-12-15 17:52:35 +0800

    2020/12/15 更新 leet4

---
+++ src/main/java/leetcode/leet4.java
@@ -0,0 +1,14 @@
+package leetcode;
+
+public class leet4 {
+    public static void main(String[] args) {
+       int num1[]={1,3};
+       int num2[]={2};
+        System.out.println(findMedianSortedArrays(num1,num2));
+    }
+
+    public static double findMedianSortedArrays(int[] nums1, int[] nums2) {
+
+        return 1;
+    }
+}
