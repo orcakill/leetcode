313cf76e0cc4d0422b61e34d90aca1e3d4ce0c2f
orcakill <orcakill@dingtalk.com>
2020-12-17 17:48:25 +0800

    2020/12/17 更新 leet4

--- src/main/java/leetcode/leet4.java
+++ src/main/java/leetcode/leet4.java
@@ -1,5 +1,9 @@
 package leetcode;
 
+import java.sql.Connection;
+import java.util.ArrayList;
+import java.util.Collections;
+
 public class leet4 {
     public static void main(String[] args) {
         int num1[]={1,3};
@@ -8,6 +12,19 @@ public class leet4 {
     }
 
     public static double findMedianSortedArrays(int[] nums1, int[] nums2) {
+        double num=0;
+        ArrayList arrayList1=new ArrayList();
+        ArrayList arrayList2=new ArrayList();
+        for(int i=0;i<nums1.length;i++){
+            arrayList1.add(nums1[i]);
+        }
+        for(int i=0;i<nums2.length;i++){
+            arrayList2.add(nums2[i]);
+        }
+        arrayList1.addAll(arrayList2);
+        Collections.sort(arrayList1);
+        int size=arrayList1.size();
+
 
         return 1;
     }
