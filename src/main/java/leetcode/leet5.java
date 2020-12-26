7cea365f4373e7d10eb416b0d42e6b001ac31a31
orcakill <orcakill@dingtalk.com>
2020-12-27 00:48:32 +0800

    2020/12/17 更新 leet4

--- src/main/java/leetcode/leet5.java
+++ src/main/java/leetcode/leet5.java
@@ -7,34 +7,31 @@ public class leet5 {
     public static void main(String[] args) {
         String s1="babad";
         String s2="vckpzcfezppubykyxvwhbwvgezvannjnnxgaqvesrhdsgngcbbdpqeodzmqbkrwekakrukwxhqjeacxhkixruwshgwkjthmtqumvqcvhhoavarlwhpzbbniqrswvyhtfquioooejsbnxdnjrfhzpdrljcuenzjpzkyrgpxrbtchnzmdkekhmuqpoljvrpndzmogeuxjotdsyrrudligpgwcblaimqdqsgmjrbvyonugzsbkdhawmewiaccuvfnpftcjdjuljekiaipknorknwyx";
-    //    System.out.println(longestPalindrome(s1));
-        System.out.println(longestPalindrome(s2));
+
+        System.out.println(longestPalindrome(s1));
+    //    System.out.println(longestPalindrome(s2));
     }
 
     public static String longestPalindrome(String s) {
-        if(s.length()==0){
-            return  null;
-        }
-       List<String> list=new ArrayList<>();
-       for(int i=0;i<s.length();i++){
-           list.add(s.substring(i,i+1));
-           for(int j=i+1;j<s.length();j++){
-               String  s1=s.substring(i,j+1);
-               StringBuffer s2=new StringBuffer(s1);
-               StringBuffer s3=s2.reverse();
-               String       s4=new String(s3);
-               if(s1.equals(s4)){
-                   list.add(s1);
-               }
-
-           }
-       }
-        int num=0;
-        for(int i=0;i<list.size()-1;i++){
-            if(list.get(num).length()<list.get(i+1).length()){
-                num=i+1;
+        int n = s.length();
+        boolean[][] dp = new boolean[n][n];
+        String ans = "";
+        for (int l = 0; l < n; ++l) {
+            for (int i = 0; i + l < n; ++i) {
+                int j = i + l;
+                if (l == 0) {
+                    dp[i][j] = true;
+                } else if (l == 1) {
+                    dp[i][j] = (s.charAt(i) == s.charAt(j));
+                } else {
+                    dp[i][j] = (s.charAt(i) == s.charAt(j) && dp[i + 1][j - 1]);
+                }
+                if (dp[i][j] && l + 1 > ans.length()) {
+                    ans = s.substring(i, i + l + 1);
+                }
             }
         }
-        return list.get(num);
+          return ans;
+
     }
 }
