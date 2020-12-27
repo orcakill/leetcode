a37617770336eaccc4b40003b63688f33f63bce4
orcakill <orcakill@dingtalk.com>
2020-12-27 19:29:37 +0800

    2020/12/27 更新 leet5

--- src/main/java/leetcode/leet5.java
+++ src/main/java/leetcode/leet5.java
@@ -7,31 +7,39 @@ public class leet5 {
     public static void main(String[] args) {
         String s1="babad";
         String s2="vckpzcfezppubykyxvwhbwvgezvannjnnxgaqvesrhdsgngcbbdpqeodzmqbkrwekakrukwxhqjeacxhkixruwshgwkjthmtqumvqcvhhoavarlwhpzbbniqrswvyhtfquioooejsbnxdnjrfhzpdrljcuenzjpzkyrgpxrbtchnzmdkekhmuqpoljvrpndzmogeuxjotdsyrrudligpgwcblaimqdqsgmjrbvyonugzsbkdhawmewiaccuvfnpftcjdjuljekiaipknorknwyx";
-
+        String s3="bb";
         System.out.println(longestPalindrome(s1));
-    //    System.out.println(longestPalindrome(s2));
+        System.out.println(longestPalindrome(s2));
+        System.out.println(longestPalindrome(s3));
     }
 
     public static String longestPalindrome(String s) {
-        int n = s.length();
-        boolean[][] dp = new boolean[n][n];
-        String ans = "";
-        for (int l = 0; l < n; ++l) {
-            for (int i = 0; i + l < n; ++i) {
-                int j = i + l;
-                if (l == 0) {
-                    dp[i][j] = true;
-                } else if (l == 1) {
-                    dp[i][j] = (s.charAt(i) == s.charAt(j));
-                } else {
-                    dp[i][j] = (s.charAt(i) == s.charAt(j) && dp[i + 1][j - 1]);
-                }
-                if (dp[i][j] && l + 1 > ans.length()) {
-                    ans = s.substring(i, i + l + 1);
-                }
-            }
+        int len=s.length();
+        if(len<2){
+            return  s;
         }
-          return ans;
+        int max=1;
+        int begin=0;
+        char[] charArray=s.toCharArray();
+       for(int i=0;i<len-1;i++){
+           for(int j=i+1;j<len;j++){
+               if(j-i+1>max&&compare(charArray,i,j)){
+                      max=j-i+1;
+                      begin=i;
+               }
+           }
+       }
+       return s.substring(begin,begin+max);
+    }
 
+    public  static  boolean  compare(char[] charArray,int left,int right){
+        while (left<right){
+            if(charArray[left]!=charArray[right]){
+                return false;
+            }
+            left++;
+            right--;
+        }
+        return  true;
     }
 }
