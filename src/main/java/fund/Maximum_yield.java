6a4be77a6ae101e58e86a744da8b6973568a6cd3
orcakill <orcakill@dingtalk.com>
2020-12-29 22:28:50 +0800

    2020/12/29 更新 Maximum_yield

--- src/main/java/fund/Maximum_yield.java
+++ src/main/java/fund/Maximum_yield.java
@@ -1,18 +1,21 @@
 package fund;
 
-import com.alibaba.fastjson.JSONObject;
+
+
+
+import org.json.JSONObject;
+import util.GetJson;
 
 import java.text.DateFormat;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
-import java.time.LocalDate;
 import java.util.*;
 
 
 public class Maximum_yield {
     //在每周一定投100元的情况下，计算周期内中的最大收益率
     //爬取网站：天天财富网
-    public static void main(String[] args) throws ParseException {
+    public static void main(String[] args) throws Exception {
         DateFormat dateFormat1 = new SimpleDateFormat("yyyy-MM-dd");
         Date ksrq= dateFormat1.parse("2020-01-01");
         Date jsrq= dateFormat1.parse("2020-12-31");
@@ -22,10 +25,10 @@ public class Maximum_yield {
         System.out.println(count(s1,ksrq,jsrq));
 
     }
-    public static String count(String s,Date ksrq,Date jsrq) throws ParseException {
+    public static String count(String s,Date ksrq,Date jsrq) throws Exception {
         List<Double> list=new ArrayList<>();
         Map<String,String> map=get_week(ksrq,jsrq);
-
+        String code=getCode(s);
         return s;
     }
 
@@ -47,17 +50,25 @@ public class Maximum_yield {
     }
 
     //获取基金的基金代码
-    public static String getCode(String s){
+    public static String getCode(String s) throws Exception {
         Map<String,String> map=new TreeMap<>();
         String address="http://fund.eastmoney.com/js/fundcode_search.js";
+        String dayLine = new GetJson().getHttpJson(address,1);
+        dayLine=dayLine.substring(11,dayLine.length());
+        dayLine=dayLine.substring(0,dayLine.length()-3);
+        dayLine=dayLine.replaceAll("\\\"", "");
+        dayLine=dayLine.replaceAll("[\\[\\]]","");
+        String substring = dayLine.substring(0, dayLine.length() - 1);
+        String[] result = substring.split(",");
+        String  s0="";
+        for(int i=0;i<result.length;i++){
+            if(result[i].equals(s)){
+                s0=result[i-1];
+            }
+        }
 
-        return  s;
+        return  s0;
     }
 
-    //解析url地址
-    public  static JSONObject getHttpJson(String url){
 
-
-        return null;
-    }
 }
