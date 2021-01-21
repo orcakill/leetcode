53304f63dfc788fd9e04b502ef8f9df54a011dc7
orcakill <orcakill@dingtalk.com>
2021-01-21 23:00:06 +0800

    update leet6

--- src/main/java/leetcode/leet6.java
+++ src/main/java/leetcode/leet6.java
@@ -16,13 +16,24 @@ public class leet6 {
 
 
     public static String convert(String s, int numRows) {
-       List<List<String>> lists=new ArrayList<>();
-       for(int i=0;i<numRows;i++){
-           for(int j=0;j<(s.length()/3);i++){
+        if (numRows == 1) return s;
 
-           }
-       }
+        List<StringBuilder> rows = new ArrayList<>();
+        for (int i = 0; i < Math.min(numRows, s.length()); i++)
+            rows.add(new StringBuilder());
 
-        return  s;
+        int curRow = 0;
+        boolean goingDown = false;
+
+        for (char c : s.toCharArray()) {
+            rows.get(curRow).append(c);
+            if (curRow == 0 || curRow == numRows - 1) goingDown = !goingDown;
+            curRow += goingDown ? 1 : -1;
+        }
+
+        StringBuilder ret = new StringBuilder();
+        for (StringBuilder row : rows) ret.append(row);
+        return ret.toString();
+
     }
 }
