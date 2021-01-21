639e0e26fc72452e160c24c574e0207b0df41774
orcakill <orcakill@dingtalk.com>
2021-01-21 19:24:27 +0800

    update leet6

---
+++ src/main/java/leetcode/leet6.java
@@ -0,0 +1,28 @@
+package leetcode;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @author orcakill
+ * @date 2021/1/21  13:30
+ **/
+public class leet6 {
+    public static void main(String[] args) throws Exception {
+        String  str="PAYPALISHIRING";
+
+        System.out.println(convert(str,3));
+    }
+
+
+    public static String convert(String s, int numRows) {
+       List<List<String>> lists=new ArrayList<>();
+       for(int i=0;i<numRows;i++){
+           for(int j=0;j<(s.length()/3);i++){
+
+           }
+       }
+
+        return  s;
+    }
+}
