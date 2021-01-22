6c1c46585b35c05f4c24cb771463e3fcfc292a32
orcakill <orcakill@dingtalk.com>
2021-01-22 17:47:02 +0800

    update FindITNews

---
+++ src/main/java/other/articles/entity/ITHomeNews.java
@@ -0,0 +1,82 @@
+package other.articles.entity;
+
+import java.util.Date;
+
+/**
+ * @author orcakill
+ * @date 2021/1/22  10:55
+ **/
+public class ITHomeNews {
+    private String newsName; /*新闻名称*/
+    private Date newsDate; /*新闻时间*/
+    private String newsTags; /*新闻标签*/
+    private String newsDescribe; /*新闻描述*/
+    private String newsSource; /*新闻来源*/
+    private Integer newsPaise; /*点赞人数*/
+    private Integer newsTread; /*践踏人数*/
+    private String newsIndex; /*新闻网址名称*/
+
+    public String getNewsIndex() {
+        return newsIndex;
+    }
+
+    public void setNewsIndex(String newsIndex) {
+        this.newsIndex = newsIndex;
+    }
+
+    public String getNewsName() {
+        return newsName;
+    }
+
+    public void setNewsName(String newsName) {
+        this.newsName = newsName;
+    }
+
+    public Date getNewsDate() {
+        return newsDate;
+    }
+
+    public void setNewsDate(Date newsDate) {
+        this.newsDate = newsDate;
+    }
+
+    public String getNewsTags() {
+        return newsTags;
+    }
+
+    public void setNewsTags(String newsTags) {
+        this.newsTags = newsTags;
+    }
+
+    public String getNewsDescribe() {
+        return newsDescribe;
+    }
+
+    public void setNewsDescribe(String newsDescribe) {
+        this.newsDescribe = newsDescribe;
+    }
+
+    public String getNewsSource() {
+        return newsSource;
+    }
+
+    public void setNewsSource(String newsSource) {
+        this.newsSource = newsSource;
+    }
+
+    public Integer getNewsPaise() {
+        return newsPaise;
+    }
+
+    public void setNewsPaise(Integer newsPaise) {
+        this.newsPaise = newsPaise;
+    }
+
+    public Integer getNewsTread() {
+        return newsTread;
+    }
+
+    public void setNewsTread(Integer newsTread) {
+        this.newsTread = newsTread;
+    }
+}
