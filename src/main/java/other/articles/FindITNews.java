f21ad3fee28b4f21528ecfcd551215ef79c895be
orcakill <orcakill@dingtalk.com>
2021-01-24 23:48:32 +0800

    update FindITNews

--- src/main/java/other/articles/FindITNews.java
+++ src/main/java/other/articles/FindITNews.java
@@ -1,14 +1,15 @@
 package other.articles;
 
 import other.articles.entity.ITHomeNews;
+import other.dao.ITNews;
 
-import java.sql.SQLException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.List;
 
 import static other.articles.map.getWebAddress.getWebAddress;
 import static other.articles.util.HttpClient.getITHttpClient;
+import static other.articles.util.getContent.getITContent;
 import static other.articles.util.getIndex.getIndex;
 import static other.articles.util.screen.screenITNews;
 
@@ -17,8 +18,10 @@ import static other.articles.util.screen.screenITNews;
  * @date 2021/1/22  11:05
  **/
 public class FindITNews {
+
     //爬取IT之家AI数据
     public  static  void  main(String[] args) throws  Exception{
+
         //爬取IT之家最近一年数据
         String web="IT之家AI";
         SimpleDateFormat format=new SimpleDateFormat("yyyy-MM-dd");
@@ -30,17 +33,28 @@ public class FindITNews {
     }
 
 
-    public  static  void  dealData(String web,Date startDate,Date endDate) throws SQLException {
+    public  static  void  dealData(String web,Date startDate,Date endDate) throws Exception {
         String url = getWebAddress(web);
         //获取当前页IT之家AI新闻目录
         //检查数据库是否已有，将已有的剔除，不执行插入
         List<ITHomeNews> itHomeNews=screenITNews(getIndex(getITHttpClient(url)));
         //获取新闻内容
+        if(itHomeNews.size()>0){
+
         for(int i=0;i<itHomeNews.size();i++){
             ITHomeNews itHomeNews1=itHomeNews.get(i);
+            String href=itHomeNews1.getNewsHref();
+            ITHomeNews itHomeNews2=getITContent(getITHttpClient(href));
+            itHomeNews2.setNewsName(itHomeNews1.getNewsName());
+            itHomeNews2.setNewsTags(itHomeNews1.getNewsTags());
+            itHomeNews2.setNewsDescribe(itHomeNews1.getNewsDescribe());
+            itHomeNews2.setNewsIndex(web);
+            itHomeNews2.setNewsHref(href);
+            itHomeNews.set(i,itHomeNews2);
+            ITNews.insert(itHomeNews2);
 
         }
 
-
+        }
     }
 }
--- src/main/java/other/articles/util/getContent.java
+++ src/main/java/other/articles/util/getContent.java
@@ -1,8 +1,40 @@
 package other.articles.util;
 
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+import other.articles.entity.ITHomeNews;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 /**
  * @author orcakill
  * @date 2021/1/23  14:26
  **/
 public class getContent {
+    public  static ITHomeNews getITContent(String html) throws ParseException {
+        ITHomeNews itHomeNews=new ITHomeNews();
+        Document document = Jsoup.parse(html);
+        String title=document.select("h1").text();
+        SimpleDateFormat format=new SimpleDateFormat("yyyy/MM/dd HH:mm:ss");
+        String date= document.getElementById("pubtime_baidu").text();
+        Date date1=format.parse(date);
+        SimpleDateFormat format1=new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+        date=format1.format(date1);
+        String  source=document.getElementById("source_baidu").select("a").text();
+        Element list =document.getElementById("paragraph");
+        Elements elements= list.select("p");
+        StringBuilder content= new StringBuilder();
+        for(Element element:elements){
+            content.append("    ").append(element.select("p").text()).append("\r\n");
+        }
+        itHomeNews.setNewsName(title);
+        itHomeNews.setNewsDate(date);
+        itHomeNews.setNewsSource(source);
+        itHomeNews.setNewsContent(content.toString());
+        return itHomeNews;
+    }
 }
