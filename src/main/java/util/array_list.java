0759c387aa5318bb44de71d7d5c72f472ec58cc8
orcakill <orcakill@dingtalk.com>
2021-01-29 08:41:55 +0800

    2021/01/28 deal sensitive history

--- src/main/java/util/array_list.java
+++ src/main/java/util/array_list.java
@@ -6,7 +6,7 @@ import java.util.List;
 
 public class array_list {
     public static List<int []>  array_list_one(int[] arr1){
-        List<int[]> tbList = Arrays.asList(arr1);
+          List<int[]> tbList = Arrays.asList(arr1);
         return  tbList;
     }
 }
