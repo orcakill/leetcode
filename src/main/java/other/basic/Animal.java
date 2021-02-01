faf460d2261fbf26d3f598911ef2fe1b1ea72839
orcakill <orcakill@dingtalk.com>
2021-02-01 17:57:23 +0800

    2021/02/01 deal sensitive history

---
+++ src/main/java/other/basic/Animal.java
@@ -0,0 +1,21 @@
+package other.basic;
+
+/**
+ * @author orcakill
+ * @date 2021/1/31  14:38
+ **/
+public class Animal {
+    private  String name;
+    private  String color;
+
+
+
+    public void eat(){
+        System.out.println("吃饭");
+    }
+    public void sleep(){
+        System.out.println("睡觉");
+    }
+}
+
+
