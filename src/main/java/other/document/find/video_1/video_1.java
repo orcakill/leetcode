6f4397a9cecf1acb8064370e0b7edfe7fc7e9179
orcakill <orcakill@dingtalk.com>
2021-05-06 19:01:21 +0800

    2021/05/06 push video_1

--- src/main/java/other/document/find/video_1/video_1.java
+++ src/main/java/other/document/find/video_1/video_1.java
@@ -1,4 +1,4 @@
-package other.document;
+package other.document.find.video_1;
 
 import java.io.*;
 import java.net.URL;
@@ -8,8 +8,8 @@ import java.net.URLConnection;
  * @author orcakill
  * @date 2021/4/15  16:48
  **/
-public class video {
-       /*爬取b站的1个视频*/
+public class video_1 {
+       /*爬取b站的1个视频,使用手动获取url*/
        public static void main(String[] args) {
            System.out.println("开始");
            long start = System.currentTimeMillis();
@@ -17,7 +17,7 @@ public class video {
             *  从json中获取到的 url
             *  请获取后手动填写
             */
-           String lastUrl = "http://upos-hz-mirrorkodou.acgvideo.com/upgcxcode/22/48/111804822/111804822-1-80.flv?e=ig8euxZM2rNcNbu37zUVhoManwuBhwdEto8g5X10ugNcXBlqNxHxNEVE5XREto8KqJZHUa6m5J0SqE85tZvEuENvNo8g2ENvNo8i8o859r1qXg8xNEVE5XREto8GuFGv2U7SuxI72X6fTr859r1qXg8gNEVE5XREto8z5JZC2X2gkX5L5F1eTX1jkXlsTXHeux_f2o859IB_&uipk=5&nbs=1&deadline=1570200690&gen=playurl&os=kodou&oi=1033761651&trid=c05e1a0ff69a47a08dbb4b39210cbd49u&platform=pc&upsig=03d0df9f30c814d21e70d4e2e1be2e17&uparams=e,uipk,nbs,deadline,gen,os,oi,trid,platform&mid=28569524";
+           String lastUrl = "https://upos-hz-mirrorakam.akamaized.net/upgcxcode/22/48/111804822/111804822_nb2-1-80.flv?e=ig8euxZM2rNcNbuM7WdVhoMa7wUVhwdEto8g5X10ugNcXBlqNxHxNEVE5XREto8KqJZHUa6m5J0SqE85tZvEuENvNo8g2ENvNo8i8o859r1qXg8xNEVE5XREto8GuFGv2U7SuxI72X6fTr859r1qXg8gNEVE5XREto8z5JZC2X2gkX5L5F1eTX1jkXlsTXHeux_f2o859IB_&uipk=5&nbs=1&deadline=1620291477&gen=playurlv2&os=akam&oi=804332179&trid=41e6ed23a5c441d096898098d08b4eaau&platform=pc&upsig=6d47a91d34af00efaf9b8ce82e3c46e1&uparams=e,uipk,nbs,deadline,gen,os,oi,trid,platform&hdnts=exp=1620291477~hmac=4a6298d916753170fe9e7358ced00d6fcded1922eff79e726df838a0d9a96262&mid=179559763&orderid=0,1&agrr=1&logo=80000000";
            //自定义文件名称
            String fileName = "a.mp4";
            downloadMovie(lastUrl, fileName);
@@ -33,7 +33,7 @@ public class video {
             URL url = new URL(BLUrl);
             URLConnection urlConnection = url.openConnection();
             // 填需要爬取的av号
-            urlConnection.setRequestProperty("Referer", "https://www.bilibili.com/video/av69924947");
+            urlConnection.setRequestProperty("Referer", "https://www.bilibili.com/video/av58906853");
             urlConnection.setRequestProperty("Sec-Fetch-Mode", "no-cors");
             urlConnection.setRequestProperty("User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/77.0.3865.90 Safari/537.36");
             urlConnection.setConnectTimeout(10 * 1000);
