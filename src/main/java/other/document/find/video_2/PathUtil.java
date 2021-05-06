c74342dd3e6c8193eb198aef98a270c8a39828ad
orcakill <orcakill@dingtalk.com>
2021-05-06 19:01:21 +0800

    2021/05/06 push video_1

---
+++ src/main/java/other/document/find/video_2/PathUtil.java
@@ -0,0 +1,28 @@
+package other.document.find.video_2;
+
+import java.io.File;
+import java.io.IOException;
+
+/**
+ * @author orcakill
+ * @date 2021/5/6  18:21
+ **/
+public class PathUtil {
+    // 创建视频路径
+    public static String createMoviePath(String title) {
+        System.out.println("开始创建视频路径");
+        //图片名称
+        String movieName = title + ".mp4";
+        //创建路径
+        String path = "F:\\test\\video_find\\";
+        File dir = new File(path);
+        if (!dir.exists()) {
+            dir.mkdirs();
+        }
+        String fileName = dir + File.separator + movieName;
+        System.out.println("视频路径：" + fileName);
+
+        return fileName;
+    }
+
+}
