204ed4cd7963b40d985af1f457e65bc951ff6e61
orcakill <orcakill@dingtalk.com>
2021-05-12 18:54:02 +0800

    2021/05/11 upload_baidu

---
+++ src/main/java/other/document/upload/upload_baidu.java
@@ -0,0 +1,12 @@
+package other.document.upload;
+
+/**
+ * @author orcakill
+ * @date 2021/5/12  11:11
+ **/
+public class upload_baidu {
+    public static void main(String[] args) {
+        String s="F:\\test\\video_compress\\a.zip";
+
+    }
+}
