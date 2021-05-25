63be1d82ed351972ed0f0d17afa6d023601e82cf
orcakill <orcakill@dingtalk.com>
2021-05-25 22:45:49 +0800

    feat(百度网盘): 获取用户信息
    根据token发送get请求获取用户信息

--- src/main/java/other/baiduNetdisk/module/userInfo.java
+++ src/main/java/other/baiduNetdisk/module/userInfo.java
@@ -1,5 +1,10 @@
 package other.baiduNetdisk.module;
 
+import other.baiduNetdisk.utils.HttpRequest;
+
+import java.net.URLEncoder;
+import java.nio.charset.StandardCharsets;
+
 import static other.baiduNetdisk.utils.diskUtils.getAuth;
 
 /**
@@ -7,7 +12,19 @@ import static other.baiduNetdisk.utils.diskUtils.getAuth;
  * @date 2021/5/24  18:46
  **/
 public class userInfo {
-      private class    getUserInfo{
-          String accessToken = getAuth();
+      /**
+       * @description: 获取用户信息
+       * @param
+       * @return: void
+       * @author: orcakill
+       * @date: 2021/5/25 11:25
+       */
+
+      public static void getUserInfo(){
+          String accessToken   = getAuth();
+
+          //发送 GET 请求
+          String s= HttpRequest.sendGet("https://pan.baidu.com/rest/2.0/xpan/nas", "access_token="+accessToken+"&method=uinfo");
+          System.out.println(s);
       }
 }
