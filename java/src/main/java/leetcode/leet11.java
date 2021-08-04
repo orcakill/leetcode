8dd60dd07df2e31f4e4b1b1cf638d074aeb74b69
orcakill <orcakill@dingtalk.com>
2021-08-04 14:53:55 +0800

    fix(leet611): leet611
    leet611

---
+++ java/src/main/java/leetcode/leet11.java
@@ -0,0 +1,12 @@
+package leetcode;
+
+public class leet11  {
+    public static void main(String[] args) {
+        int[] a1 = {2, 6, 4, 8, 10, 9, 15};
+        System.out.println(maxArea(a1));
+    }
+
+    public static int maxArea(int[] height) {
+        return 1;
+    }
+}
