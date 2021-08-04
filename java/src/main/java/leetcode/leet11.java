883a2f38b95be36d84dbc9913e6be801a3c90559
orcakill <orcakill@dingtalk.com>
2021-08-04 19:02:37 +0800

    fix(leet611): leet611
    leet611

--- java/src/main/java/leetcode/leet11.java
+++ java/src/main/java/leetcode/leet11.java
@@ -2,11 +2,29 @@ package leetcode;
 
 public class leet11  {
     public static void main(String[] args) {
-        int[] a1 = {2, 6, 4, 8, 10, 9, 15};
+        int[] a1 = {1,8,6,2,5,4,8,3,7};
+        int[] b1={1,1};
+        int[] c1={4,3,2,1,4};
+        int[] d1={1,2,1};
         System.out.println(maxArea(a1));
+        System.out.println(maxArea(b1));
+        System.out.println(maxArea(c1));
+        System.out.println(maxArea(d1));
     }
 
     public static int maxArea(int[] height) {
-        return 1;
+        int l = 0, r = height.length - 1;
+        int ans = 0;
+        while (l < r) {
+            int area = Math.min(height[l], height[r]) * (r - l);
+            ans = Math.max(ans, area);
+            if (height[l] <= height[r]) {
+                ++l;
+            }
+            else {
+                --r;
+            }
+        }
+        return ans;
     }
 }
