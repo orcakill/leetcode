8043bd0340f5f5087429c3c96c678ba5d594f1bf
orcakill <orcakill@dingtalk.com>
2021-08-06 17:31:24 +0800

    fix(leet847): leet847
    leet847

---
+++ java/src/main/java/leetcode/leet847.java
@@ -0,0 +1,15 @@
+package leetcode;
+
+public class leet847 {
+    public static void main(String[] args) {
+        int[][] a1 = {{1,2,3},{0},{0},{0}};
+        System.out.println(shortestPathLength(a1));
+
+    }
+
+    public static int shortestPathLength(int[][] graph) {
+
+        return 1;
+    }
+
+}
