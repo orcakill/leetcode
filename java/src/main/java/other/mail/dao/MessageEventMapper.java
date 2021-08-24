077aa721442260b0bd2374ea310c36e7f4855e59
orcakill <orcakill@dingtalk.com>
2021-08-24 08:00:30 +0800

    feat(JDBCmapper自动化生成语句): JDBCmapper自动化完成
    JDBCmapper自动化完成

--- java/src/main/java/other/mail/dao/MessageEventMapper.java
+++ java/src/main/java/other/mail/dao/MessageEventMapper.java
@@ -1,4 +1,13 @@
 package other.mail.dao;
 
+import org.apache.log4j.LogManager;
+import org.apache.log4j.Logger;
+import other.dao.Jdbc;
+import other.mail.model.entity.MessageEventPO;
+
+import java.math.BigDecimal;
+import java.sql.*;
+
 public class MessageEventMapper {
+
 }
