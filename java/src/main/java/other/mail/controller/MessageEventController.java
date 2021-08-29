37ff235039e06cb61a7f3e9db1180baac2379b8b
orcakill <orcakill@dingtalk.com>
2021-08-30 00:18:10 +0800

    fix(邮件推送): 邮件推送-github历史提交信息获取
    邮件推送-github历史提交信息获取

--- java/src/main/java/other/mail/controller/MessageEventController.java
+++ java/src/main/java/other/mail/controller/MessageEventController.java
@@ -2,6 +2,7 @@ package other.mail.controller;
 
 import com.alibaba.fastjson.JSONArray;
 import com.alibaba.fastjson.JSONObject;
+import other.mail.model.dto.CommitDTO;
 import other.mail.model.entity.MessageEventPO;
 import other.mail.util.GetJson;
 
@@ -15,14 +16,35 @@ public class MessageEventController {
 		List<MessageEventPO> messageEventPOList = new ArrayList<> ();
 		Calendar calendar = Calendar.getInstance ();
 		int week = calendar.get (Calendar.DAY_OF_WEEK);
-		if (week != 1) {
-			/*leetcode需要一周一次提交一次leetcode题库的代码*/
-			MessageEventPO messageEventPO=new MessageEventPO ();
-			String  url="https://api.github.com/repos/orcakill/leetcode/commits";
-			JSONObject jsonObject= GetJson.getHttpJson (url);
-			messageEventPO.setMessageTitle ("leetcode题目本周未提交");
-			messageEventPO.setMessageContent ("");
+		
+		/*leetcode需要一周一次提交一次leetcode题库的代码*/
+		MessageEventPO messageEventPO = new MessageEventPO ();
+		String url = "https://api.github.com/repos/orcakill/leetcode/commits?per_page=100&page=";
+		List<CommitDTO> commitDTOS=new ArrayList<> ();
+		int num=1;
+		while(true){
+			String  str= GetJson.getHttpJson (url+num);
+			JSONArray  jsonArray=JSONArray.parseArray (str);
+			if(jsonArray.size ()==0){
+				break;
+			}
+			for(int i=0;i<jsonArray.size ();i++){
+				JSONObject jsonObject=jsonArray.getJSONObject (i);
+				JSONObject jsonObject1=jsonObject.getJSONObject ("commit");
+				JSONObject jsonObject2=jsonObject1.getJSONObject ("committer");
+				Date date=jsonObject2.getDate ("date");
+				String content=jsonObject1.getString ("message");
+				CommitDTO commitDTO=new CommitDTO ();
+				commitDTO.setCommitDate (date);
+				commitDTO.setCommitContent (content);
+				commitDTOS.add (commitDTO);
+			}
+			num++;
+			
 		}
+		messageEventPO.setMessageTitle ("leetcode题目本周未提交");
+		messageEventPO.setMessageContent ("");
+		
 		return messageEventPOList;
 	}
 }
