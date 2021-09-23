e4c182b733bbdf0bbbcdead7799e2231e8f7c3f7
orcakill <orcakill@dingtalk.com>
2021-09-24 00:07:04 +0800

    feat(JDBCMapper自动化生成): JDBCMapper自动化生成完善
    JDBCMapper自动化生成完善

--- java/src/main/java/other/auto/method/DeleteAll.java
+++ java/src/main/java/other/auto/method/DeleteAll.java
@@ -8,15 +8,10 @@ public class DeleteAll {
 	public static  StringBuilder deleteAll(String name, String name1, String table, List<ClassPO> classPOS,
 	                                  List<ClassPO> classPOS1,List<ClassPO> classPOS2) {
 		StringBuilder str = new StringBuilder ();
-		str.append ("/*删除方法*/\r\n public static void deleteAll () throws SQLException {\r\n")
-		   .append ("\t\tString sql = \"delete   from ")
-		   .append (table)
-		   .append ("\";\r\n")
-		   .append ("\t\tConnection connection = Jdbc.getConnection ();\r\n")
-		   .append ("\t\tPreparedStatement preparedStatement = connection.prepareStatement (sql);\r\n")
-		   .append ("\t\tint num = preparedStatement.executeUpdate ();\r\n")
-		   .append ("\t\tif (num > 0) {\n\t\t\tlogger.info (\"删除成功\");\n\t\t}\n\t\telse {\n\t\t\tlogger.error " +
-		            "(\"删除失败\");\n\t\t}\n\t\tJdbc.release (null, preparedStatement, connection);\n\t}");
+		str.append("\tpublic static void deleteAll () throws SQLException {\n" +
+		           "\t\tString sql = \"delete  from task_list\";\n" +
+		           "\t\tSql.deleteSQL (sql, (org.apache.logging.log4j.core.Logger) logger);\n" +
+		           "\t}");
 	     return  str;
 	}
 }
