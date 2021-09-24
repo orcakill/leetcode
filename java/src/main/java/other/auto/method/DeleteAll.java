d0c67e63d24fd79cc580528f9740d2c0dd255fb0
orcakill <orcakill@dingtalk.com>
2021-09-24 22:16:50 +0800

    fix(mapper自动化完善): mapper自动化完善
    mapper自动化完善

--- java/src/main/java/other/auto/method/DeleteAll.java
+++ java/src/main/java/other/auto/method/DeleteAll.java
@@ -8,10 +8,10 @@ public class DeleteAll {
 	public static  StringBuilder deleteAll(String name, String name1, String table, List<ClassPO> classPOS,
 	                                  List<ClassPO> classPOS1,List<ClassPO> classPOS2) {
 		StringBuilder str = new StringBuilder ();
-		str.append("\tpublic static void deleteAll () throws SQLException {\n" +
-		           "\t\tString sql = \"delete  from task_list\";\n" +
-		           "\t\tSql.deleteSQL (sql, (org.apache.logging.log4j.core.Logger) logger);\n" +
-		           "\t}");
+		str.append("/*删除全部数据*/\npublic static void deleteAll () throws SQLException {\n" +
+		           "\tString sql = \"delete  from task_list\";\n" +
+		           "\tSql.deleteSQL (sql, (org.apache.logging.log4j.core.Logger) logger);\n" +
+		           "}\r\n\r\n");
 	     return  str;
 	}
 }
--- java/src/main/java/other/auto/method/DeleteOne.java
+++ java/src/main/java/other/auto/method/DeleteOne.java
@@ -4,11 +4,48 @@ import other.auto.entity.ClassPO;
 
 import java.util.List;
 
+import static other.auto.util.CommonUtils.*;
+import static other.auto.util.CommonUtils.toUpper;
+
 public class DeleteOne {
-	public static  StringBuilder deleteById(String name, String name1, String table, List<ClassPO> classPOS,
-	                                       List<ClassPO> classPOS1,List<ClassPO> classPOS2) {
-	StringBuilder str=new StringBuilder ();
-	
-	return  str;
+	public static StringBuilder deleteById (String name, String name1, String table, List<ClassPO> classPOS,
+	                                        List<ClassPO> classPOS1, List<ClassPO> classPOS2) {
+		StringBuilder str = new StringBuilder ();
+		str.append ("/*根据主键删除数据*/\npublic static void deleteById(");
+		getId (classPOS, str);
+		str.append (") throws SQLException {\r\n")
+		   .append ("\tString sql = \"delete  from ")
+		   .append (table)
+		   .append (" where ");
+		for (int i = 0; i < classPOS1.size (); i++) {
+			str.append (toLowerLine (classPOS1.get (i)
+			                                  .getName ()))
+			   .append ("=? ");
+			if (i < classPOS1.size () - 1) {
+				str.append (" and ");
+			}
+		}
+		str.append ("\";\r\n")
+		   .append ("\tConnection connection = Jdbc.getConnection ();\n" +
+		            "\tPreparedStatement preparedStatement = connection.prepareStatement (sql);\n");
+		for(int i=0;i<classPOS1.size ();i++){
+			str.append ("\tpreparedStatement.set")
+			   .append ("Object")
+			   .append ("(")
+			   .append (i + 1)
+			   .append (",")
+			   .append (toLower (name)+".get"+toUpper (classPOS1.get (i).getName ())+"());")
+			   .append ("\r\n");
+		}
+		str.append ("\tint num = preparedStatement.executeUpdate ();\n" +
+		            "\tif (num > 0) {\n" +
+		            "\t\tlogger.info (\"删除成功\");\n" +
+		            "\t}\n" +
+		            "\telse {\n" +
+		            "\t\tlogger.error (\"删除失败\");\n" +
+		            "\t}\n" +
+		            "\tJdbc.release (null, preparedStatement, connection);\n" +
+		            "}");
+		return str;
 	}
 }
