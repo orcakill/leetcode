17848cda7c94d376c26898d6c0932981adde6e99
orcakill <orcakill@dingtalk.com>
2021-09-27 18:50:32 +0800

    fix(mapper自动化完善): mapper自动化完善
    mapper自动化完善

---
+++ java/src/main/java/other/auto/method/CheckTest.java
@@ -0,0 +1,29 @@
+package other.auto.method;
+
+import other.auto.entity.ClassPO;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.List;
+
+import static other.auto.util.CommonUtils.*;
+
+public class CheckTest {
+	public static StringBuilder checkTest (String name, String name1, List<ClassPO> classPOS,
+	                                          List<ClassPO> classPOS1) {
+	StringBuilder stringBuilder=new StringBuilder ();
+		Date date = new Date ();
+		SimpleDateFormat simpleDateFormat=new SimpleDateFormat ("yyyy-MM-dd");
+		String  strDate=simpleDateFormat.format (date);
+		stringBuilder.append ("@Test\r\n");
+		stringBuilder.append ("public void check() throws SQLException {\n")
+				.append ("\tBoolean boolean=check(");
+		getParamExample (classPOS1, stringBuilder, strDate);
+		stringBuilder.append (");\n");
+		stringBuilder.append ("\tSystem.out.println (boolean);\r\n")
+				.append ("}\n");
+	return  stringBuilder;
+	}
+	
+
+}
