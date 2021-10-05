f0a193c0f482890bfd6efe40499f712e7ee043d0
orcakill <orcakill@dingtalk.com>
2021-10-05 22:47:17 +0800

    feat(leet17): leet17
    leet17

--- java/src/main/java/leetcode/leet17.java
+++ java/src/main/java/leetcode/leet17.java
@@ -1,7 +1,9 @@
 package leetcode;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class leet17 {
     public static void main(String[] args) {
@@ -9,25 +11,42 @@ public class leet17 {
         System.out.println(letterCombinations(s));
     }
 
+
     public static List<String> letterCombinations(String digits) {
+            List<String> combinations = new ArrayList<String>();
+            if (digits.length() == 0) {
+                return combinations;
+            }
+            Map<Character, String> phoneMap = new HashMap<Character, String> () {{
+                put('2', "abc");
+                put('3', "def");
+                put('4', "ghi");
+                put('5', "jkl");
+                put('6', "mno");
+                put('7', "pqrs");
+                put('8', "tuv");
+                put('9', "wxyz");
+            }};
+            backtrack(combinations, phoneMap, digits, 0, new StringBuffer());
+            return combinations;
+        }
+    
+        public static void backtrack(List<String> combinations, Map<Character, String> phoneMap, String digits, int index,
+                               StringBuffer combination) {
+            if (index == digits.length()) {
+                combinations.add(combination.toString());
+            } else {
+                char digit = digits.charAt(index);
+                String letters = phoneMap.get(digit);
+                int lettersCount = letters.length();
+                for (int i = 0; i < lettersCount; i++) {
+                    combination.append(letters.charAt(i));
+                    backtrack(combinations, phoneMap, digits, index + 1, combination);
+                    combination.deleteCharAt(index);
+                }
+            }
+        }
 
-        return new ArrayList<>();
-
-    }
-    public static  List<String> abc(String s){
-        ArrayList<String> l1 = new ArrayList<>();
-        String s2 = "abc";
-        String s3 = "def";
-        String s4 = "ghi";
-        String s5 = "jkl";
-        String s6 = "mno";
-        String s7 = "pqrs";
-        String s8 = "tuv";
-        String s9 = "wxyz";
-
-        return  l1;
-
-    }
 
 
 
