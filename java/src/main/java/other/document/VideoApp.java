4a97f1f7943ee8975f7e57b8d49cbb32a5045d3b
orcakill <orcakill@dingtalk.com>
2021-11-01 18:21:12 +0800

    feat(视频解压缩): 视频解压缩
    视频解压缩

---
+++ java/src/main/java/other/document/VideoApp.java
@@ -0,0 +1,13 @@
+package other.document;
+
+/**
+ * @author orcakill
+ * @version 1.0.0
+ * @ClassName VideoApp.java
+ * @Description TODO
+ * @createTime 2021年11月01日 18:01:00
+ */
+public class VideoApp {
+//	通过url获取视频
+//	将视频转换为480*320
+}
