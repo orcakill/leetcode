45694364c7006da79390199f4b2db2832c36617e
orcakill <orcakill@dingtalk.com>
2022-05-09 18:40:26 +0800

    feat(菜鸟教程-python): 2.python基本数据类型

--- python/runoob/2、python基本数据类型.py
+++ python/runoob/2、python基本数据类型.py
@@ -5,3 +5,50 @@ name    = "runoob"     # 字符串
 print (counter)
 print (miles)
 print (name)
+
+
+
+list = [ 'abcd', 786 , 2.23, 'runoob', 70.2 ]
+tinylist = [123, 'runoob']
+
+print (list)            # 输出完整列表
+print (list[0])         # 输出列表第一个元素
+print (list[1:3])       # 从第二个开始输出到第三个元素
+print (list[2:])        # 输出从第三个元素开始的所有元素
+print (tinylist * 2)    # 输出两次列表
+print (list + tinylist) # 连接列表
+
+
+def reverseWords(input):
+    # 通过空格将字符串分隔符，把各个单词分隔为列表
+    inputWords = input.split(" ")
+
+    # 翻转字符串
+    # 假设列表 list = [1,2,3,4],
+    # list[0]=1, list[1]=2 ，而 -1 表示最后一个元素 list[-1]=4 ( 与 list[3]=4 一样)
+    # inputWords[-1::-1] 有三个参数
+    # 第一个参数 -1 表示最后一个元素
+    # 第二个参数为空，表示移动到列表末尾
+    # 第三个参数为步长，-1 表示逆向
+    inputWords = inputWords[-1::-1]
+
+    # 重新组合字符串
+    output = ' '.join(inputWords)
+
+    return output
+
+
+if __name__ == "__main__":
+    input = 'I like runoob'
+    rw = reverseWords(input)
+    print(rw)
+
+tuple = ( 'abcd', 786 , 2.23, 'runoob', 70.2  )
+tinytuple = (123, 'runoob')
+
+print (tuple)             # 输出完整元组
+print (tuple[0])          # 输出元组的第一个元素
+print (tuple[1:3])        # 输出从第二个元素开始到第三个元素
+print (tuple[2:])         # 输出从第三个元素开始的所有元素
+print (tinytuple * 2)     # 输出两次元组
+print (tuple + tinytuple) # 连接元组
