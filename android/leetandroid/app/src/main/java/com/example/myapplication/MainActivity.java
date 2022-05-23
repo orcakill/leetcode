a9f6f313f0b998e94b7c7fbd9e65fb4dd7a7966f
orcakill <orcakill@dingtalk.com>
2022-05-23 23:41:24 +0800

    feat(android学习): android学习textview
    android学习

--- android/leetandroid/app/src/main/java/com/example/myapplication/MainActivity.java
+++ android/leetandroid/app/src/main/java/com/example/myapplication/MainActivity.java
@@ -2,13 +2,18 @@ package com.example.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
+    @SuppressLint("SetTextI18n")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        TextView tv_one=findViewById(R.id.tv_one);
+        tv_one.setText("leo");
     }
 }
