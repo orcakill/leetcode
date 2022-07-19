7fa733791ccff55bde52363c661654f58b600b12
orcakill <orcakill@dingtalk.com>
2022-07-19 14:38:36 +0800

    feat(java-游戏脚本调整): java-游戏脚本调整
    java-游戏脚本调整

--- java/java/src/main/java/other/dao/Sql.java
+++ java/java/src/main/java/other/dao/Sql.java
@@ -10,7 +10,7 @@ import java.text.SimpleDateFormat;
 
 public class Sql {
 	public static void deleteSQL (String sql, Logger logger) throws SQLException {
-		Connection connection = JDBC.getConnection ();
+		Connection connection = Jdbc.getConnection ();
 		PreparedStatement preparedStatement = connection.prepareStatement (sql);
 		int num = preparedStatement.executeUpdate ();
 		if (num > 0) {
@@ -19,18 +19,18 @@ public class Sql {
 		else {
 			logger.error ("删除失败");
 		}
-		JDBC.release (null, preparedStatement, connection);
+		Jdbc.release (null, preparedStatement, connection);
 	}
 	
 	public static Boolean BooleanSQL (String sql) throws SQLException {
-		Connection connection= JDBC.getConnection ();
+		Connection connection= Jdbc.getConnection ();
 		Statement statement=connection.createStatement ();
 		ResultSet resultSet =statement.executeQuery (sql);
 		int num=0;
 		while(resultSet.next()){
 			num=resultSet.getInt("num");
 		}
-		JDBC.release(null, statement, connection);
+		Jdbc.release (null, statement, connection);
 		return num!=0;
 	}
 	
