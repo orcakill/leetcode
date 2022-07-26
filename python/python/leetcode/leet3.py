2d2c67725b21eeb578aeeec8afb302dc29b72e29
orcakill <orcakill@dingtalk.com>
2022-07-26 15:11:54 +0800

    feat(python-leet3): python-leet3
    python-leet3

--- python/python/leetcode/leet3.py
+++ python/python/leetcode/leet3.py
@@ -15,24 +15,22 @@ def lengthOfLongestSubstring_1(s: str) -> int:
     return max(list)
 
 
-def length_of_longest_substring_2(s: str) -> int:
-    """
-    给定一个字符串 s ，请你找出其中不含有重复字符的 最长子串 的长度-滑动窗口，字符串不带重复
-    """
+def length_of_longest_substring_3(s: str) -> int:
+    # 给定一个字符串s,请你找出其中不含重复字符的最长子串的长度-滑动窗口，字符串不重复
     # 哈希集合，记录每个字符是否出现过
     occ = set()
     n = len(s)
-    # 右指针，初始值为 -1，相当于我们在字符串的左边界的左侧，还没有开始移动
+    # 右指针，初始值为-1，相当与我们再字符串的左边界的左侧，还没有开始移动
     rk, ans = -1, 0
     for i in range(n):
         if i != 0:
-            # 左指针向右移动一格，移除一个字符，倒序删除
+            # 左指针向右移动一格，移除一格字符，倒序删除
             occ.remove(s[i - 1])
         while rk + 1 < n and s[rk + 1] not in occ:
-            # 不断地移动右指针，扩充字符
+            # 不断移动右指针，扩充字符
             occ.add(s[rk + 1])
             rk += 1
-        # 第 i 到 rk 个字符是一个极长的无重复字符子串
+        # 第i到rk个字符是个极长的无重复字符子串
         ans = max(ans, rk - i + 1)
     return ans
 
@@ -42,6 +40,6 @@ if __name__ == '__main__':
     print(lengthOfLongestSubstring_1("bbbbb"))
     print(lengthOfLongestSubstring_1("pwwkew"))
 
-    print(length_of_longest_substring_2("abcabcbb"))
-    print(length_of_longest_substring_2("bbbbb"))
-    print(length_of_longest_substring_2("pwwkew"))
+    print(length_of_longest_substring_3("abcabcbb"))
+    print(length_of_longest_substring_3("bbbbb"))
+    print(length_of_longest_substring_3("pwwkew"))
