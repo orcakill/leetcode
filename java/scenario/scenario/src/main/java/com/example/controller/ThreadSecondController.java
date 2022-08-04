e2e60d9bc12b716f1956622afbcb8ee5ed9415a8
orcakill <orcakill@dingtalk.com>
2022-08-04 14:49:41 +0800

    feat(java-游戏脚本重构): java-游戏脚本重构
    java-游戏脚本重构

---
+++ java/scenario/scenario/src/main/java/com/example/controller/ThreadSecondController.java
@@ -0,0 +1,28 @@
+package com.example.controller;
+
+import lombok.SneakyThrows;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import java.util.Scanner;
+
+public class ThreadSecondController extends Thread {
+	public  static  final Logger logger = LogManager.getLogger ("ThreadSecondController");
+	
+	@SneakyThrows
+	public void run () {
+		logger.info ("1 阴阳寮结界+个人结界+魂土40次");
+		logger.info ("2 个人结界+魂土60次");
+		logger.info ("3 个人结界+业原火100张");
+		logger.info ("4 个人结界+日轮之陨50次");
+		logger.info ("5 个人结界+永生之海30次");
+		logger.info ("6 御灵60次");
+		logger.info ("7 斗技30次");
+		logger.info ("8 为崽而战30次");
+		Scanner scanner=new Scanner(System.in);
+		logger.info ("输入一个轮次");
+		int a=scanner.nextInt();//输入一个轮次
+		logger.info ("输入一个选项");
+		int b=scanner.nextInt();//输入一个选项
+	}
+}
--- java/scenario/scenario/src/main/java/com/example/service/ImageService.java
+++ java/scenario/scenario/src/main/java/com/example/service/ImageService.java
@@ -11,20 +11,28 @@ public interface ImageService {
 	
 	
 	/*多张图片后台识别,参数默认，识别次数，识别起始间隔，识别结束间隔，是否返回日志信息,点击*/
-	static void imagesClickBack (String folder) throws InterruptedException, AWTException {
-		imagesClickBack (folder,"夜神模拟器",60,1,2,true,true);
+	static boolean imagesClickBack (String folder) throws InterruptedException, AWTException {
+		return  imagesClickBack (folder,"夜神模拟器",60,1,2,true,true);
+	}
+	
+	static boolean imagesClickBack (String folder,Integer re_num) throws InterruptedException, AWTException {
+		return  imagesClickBack (folder,"夜神模拟器",re_num,1,2,true,true);
 	}
 	
 	/*多张图片后台识别,参数默认，识别次数，识别起始间隔，识别结束间隔，是否返回日志信息,不点击*/
-	static void imagesClickBackIsEmpty (String folder) throws InterruptedException, AWTException {
-		imagesClickBack (folder,"夜神模拟器",60,1,2,true,false);
+	static  boolean imagesClickBackIsEmpty (String folder) throws InterruptedException, AWTException {
+		return imagesClickBack (folder,"夜神模拟器",60,1,2,true,false);
+	}
+	
+	static  boolean imagesClickBackIsEmpty (String folder,Integer re_num) throws InterruptedException, AWTException {
+		return imagesClickBack (folder,"夜神模拟器",re_num,1,2,true,false);
 	}
 	
 	/*多张图片后台识别,手动设置参数*/
-	static void imagesClickBack (String folder,String process,Integer re_num,Integer start_time,
+	static boolean imagesClickBack (String folder,String process,Integer re_num,Integer start_time,
 	                             Integer end_time,Boolean boole,boolean isClick) throws InterruptedException,
 	                                                                                 AWTException {
-		ImageServiceImpl.imagesClickBack (folder,process,re_num,start_time,end_time,boole,isClick);
+		return  ImageServiceImpl.imagesClickBack (folder,process,re_num,start_time,end_time,boole,isClick);
 	}
 	
 }
