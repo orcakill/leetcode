c5189767cae6cd48ed64cc8a8f297f9f073fc3a2
orcakill <orcakill@dingtalk.com>
2022-08-04 18:48:58 +0800

    feat(java-游戏脚本重构): java-游戏脚本重构
    java-游戏脚本重构

--- java/scenario/scenario/src/main/java/com/example/controller/ThreadSecondController.java
+++ java/scenario/scenario/src/main/java/com/example/controller/ThreadSecondController.java
@@ -19,10 +19,14 @@ public class ThreadSecondController extends Thread {
 		logger.info ("6 御灵60次");
 		logger.info ("7 斗技30次");
 		logger.info ("8 为崽而战30次");
+		logger.info ("9 小号刷大号协战");
 		Scanner scanner=new Scanner(System.in);
 		logger.info ("输入一个轮次");
 		int a=scanner.nextInt();//输入一个轮次
 		logger.info ("输入一个选项");
 		int b=scanner.nextInt();//输入一个选项
+		//  进入登录界面
+		
+     	//	默认大号登录，选项为9则启用小号登录，小号登录
 	}
 }
