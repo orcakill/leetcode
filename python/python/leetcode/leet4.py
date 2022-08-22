fcc55dda930c036b2ddb5c1b19db27dbfc31b7e8
orcakill <orcakill@dingtalk.com>
2022-08-22 11:26:08 +0800

    feat(python-leet4): python-leet4
    python-leet4

--- python/python/leetcode/leet4.py
+++ python/python/leetcode/leet4.py
@@ -1,16 +1,18 @@
+import math
 from typing import List
 
 
 def find_median_sorted_arrays(self, nums1: List[int], nums2: List[int]) -> float:
-    nums1=nums1+nums2;
+    nums1 = nums1 + nums2
     nums1.sort()
-    x=len(nums1)/2
-
-
-    return 1
+    x = math.ceil(len(nums1) / 2)-1
+    if len(nums1) % 2 == 0:
+        return (nums1[x] + nums1[x + 1]) / 2
+    else:
+        return nums1[x]
 
 
 if __name__ == '__main__':
     nums1 = [1, 3]
     nums2 = [2]
-    print(find_median_sorted_arrays(nums1, nums2))
+    print(find_median_sorted_arrays(0, nums1, nums2))
