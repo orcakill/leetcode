870d779cdbf5971fa0b7f8140506708039c4dc49
orcakill <orcakill@dingtalk.com>
2022-08-29 18:27:23 +0800

    feat(java-游戏脚本2.0): java-游戏脚本2.0
    java-游戏脚本2.0

--- java/scenario/scenario/src/main/java/com/example/model/enums/SoulEnums.java
+++ java/scenario/scenario/src/main/java/com/example/model/enums/SoulEnums.java
@@ -14,11 +14,27 @@ public enum SoulEnums {
 	pet("scenario/御魂/宠物奖励"),
 	//御魂图标
 	icon("scenario/御魂/御魂图标"),
+	//御魂加成
+	soul_addition("scenario/御魂/御魂加成"),
+	//拒接协战
+	refuse_XZ("scenario/御魂/拒接协战"),
+	//挑战
+	fight("scenario/御魂/挑战"),
+	//胜利
+	win("scenario/御魂/胜利"),
+	//角色头像
+	soul_JSTX("scenario/御魂/胜利"),
+	//退出挑战
+	back_fight("scenario/御魂/退出挑战"),
 	
 	//御魂类型-八岐大蛇
 	type_BQDS("scenario/御魂/御魂类型/八岐大蛇"),
 	//御魂类型-业原火
 	type_YYH("scenario/御魂/御魂类型/业原火"),
+	//御魂类型-日轮之陨
+	type_RLZY("scenario/御魂/御魂类型/日轮之陨"),
+	//御魂类型-永生之海
+	type_YSZH("scenario/御魂/御魂类型/日轮之陨"),
 	
 	//层数-魂十一
 	type_HSY("scenario/御魂/层数/魂十一"),
@@ -26,7 +42,10 @@ public enum SoulEnums {
 	type_HS("scenario/御魂/层数/魂十"),
 	//层数-业原火三层
 	type_YYHSC("scenario/御魂/层数/业原火三层"),
-	
+	//层数-日轮之陨三层
+	type_RLZYSC("scenario/御魂/层数/日轮之陨三层"),
+	//层数-永生之海四层
+	type_YSZHSC("scenario/御魂/层数/永生之海四层"),
 	;
 	
 	SoulEnums(String value) {
