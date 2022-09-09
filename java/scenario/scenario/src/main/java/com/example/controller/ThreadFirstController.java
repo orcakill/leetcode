3e0a7dae56518498cf000f389115c69dbb0b3061
orcakill <orcakill@dingtalk.com>
2022-09-09 18:11:44 +0800

    feat(java-游戏脚本2.0): java-游戏脚本2.0
    java-游戏脚本2.0

--- java/scenario/scenario/src/main/java/com/example/controller/ThreadFirstController.java
+++ java/scenario/scenario/src/main/java/com/example/controller/ThreadFirstController.java
@@ -2,12 +2,15 @@ package com.example.controller;
 
 import com.example.service.ImageService;
 import lombok.SneakyThrows;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 import static com.example.App.ThreadSecondIsAlive;
 import static com.example.App.logger;
+import static com.example.service.MailService.sendMail;
 
 public class ThreadFirstController extends Thread {
-	
+	public  static  final Logger logger = LogManager.getLogger ("ThreadFirstController");
 	@SneakyThrows
 	public void run () {
 		System.out.println ("拒接协战");
@@ -19,7 +22,8 @@ public class ThreadFirstController extends Thread {
 			Thread.sleep (60 * 1000);
 			b = ImageService.imagesClickBackIsEmpty (file2,1,false);
 			if (b) {
-				System.out.println ("异常退出");
+				logger.info ("异常退出");
+				sendMail();
 				System.exit (0);
 			}
 			
