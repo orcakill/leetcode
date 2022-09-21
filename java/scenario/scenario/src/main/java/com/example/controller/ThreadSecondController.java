931f8c4689935569213ebb35b59d8948bcaff0aa
orcakill <orcakill@dingtalk.com>
2022-09-21 18:21:44 +0800

    feat(java-游戏脚本2.0): java-游戏脚本2.0
    java-游戏脚本2.0

--- java/scenario/scenario/src/main/java/com/example/controller/ThreadSecondController.java
+++ java/scenario/scenario/src/main/java/com/example/controller/ThreadSecondController.java
@@ -24,7 +24,7 @@ public class ThreadSecondController extends Thread {
 		logger.info ("5 个人结界+永生之海30次");
 		logger.info ("6 御灵60次");
 		logger.info ("7 斗技30次");
-		logger.info ("8 为崽而战30次");
+		logger.info ("8 当前活动战斗");
 		logger.info ("9 小号刷大号协战");
 		logger.info ("10 阴阳寮结界");
 		logger.info ("11 阴阳寮结界+斗技5次");
