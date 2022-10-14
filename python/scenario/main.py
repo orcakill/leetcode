dff059d4fc098648b98cd453a640c812ef3856d5
orcakill <orcakill@dingtalk.com>
2022-10-14 17:50:44 +0800

    feat(python-scenario): 线程学习
    线程学习

--- python/scenario/main.py
+++ python/scenario/main.py
@@ -1,27 +1,30 @@
-from loguru import logger
+from resources import my_logger
 import threading
 import time
+
+from resources.my_logger import MyLogger
 
 
 class FirstThread:
 
     def first_thread(self):
-        logger.info("线程%s运行开始" % self)
+        my_logger.info("线程%s运行开始" % self)
         time.sleep(2)
-        logger.info("线程%s运行结束" % self)
+        my_logger.info("线程%s运行结束" % self)
 
 
 class SecondThread:
 
     def second_thread(self):
-        logger.info("线程%s运行开始" % self)
+        my_logger.info("线程%s运行开始" % self)
         time.sleep(2)
-        logger.info("线程%s运行结束" % self)
+        my_logger.info("线程%s运行结束" % self)
 
 
 # 按间距中的绿色按钮以运行脚本。
 if __name__ == '__main__':
-    t1 = threading.Thread(name='1', target=FirstThread.first_thread, args=(1,))
-    t2 = threading.Thread(name='2', target=SecondThread.second_thread, args=(2,))
+    my_logger = MyLogger().get_logger()
+    t1 = threading.Thread(target=FirstThread.first_thread, args=(1,))
+    t2 = threading.Thread(target=SecondThread.second_thread, args=(2,))
     t1.start()
     t2.start()
