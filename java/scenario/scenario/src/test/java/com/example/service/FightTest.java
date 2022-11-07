298fe898680d6c15512465579c5ba9a2bac1017e
orcakill <orcakill@dingtalk.com>
2022-11-07 23:55:36 +0800

    feat(java-游戏脚本-御魂整理测试类): java-游戏脚本-御魂整理测试类
    java-游戏脚本-御魂整理测试类

---
+++ java/scenario/scenario/src/test/java/com/example/service/FightTest.java
@@ -0,0 +1,31 @@
+package com.example.service;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.junit.Test;
+
+import java.io.IOException;
+
+/**
+ * @Classname FightTest
+ * @Description TODO
+ * @Date 2022/11/7 22:57
+ * @Created by orcakill
+ */
+public class FightTest {
+	public static final Logger logger = LogManager.getLogger ("FightTest");
+	
+	public static boolean b1 = true;//是否开启测试
+	
+	//御魂强化-提升速度
+	@Test
+	public void FightTest1 () throws IOException {
+		logger.info ("御魂整理开始");
+		if (b1) {
+		      //循环强化速度御魂
+		      for(int i=1;i<=1;i++){
+				  
+		      }
+		}
+	}
+}
