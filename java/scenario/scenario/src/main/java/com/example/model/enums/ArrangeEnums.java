13489eafee199f1ed9afb705dcc879630068ec18
orcakill <orcakill@dingtalk.com>
2022-11-08 22:51:51 +0800

    feat(java-游戏脚本-御魂整理测试类): java-游戏脚本-御魂整理测试类
    java-游戏脚本-御魂整理测试类

--- java/scenario/scenario/src/main/java/com/example/model/enums/ArrangeEnums.java
+++ java/scenario/scenario/src/main/java/com/example/model/enums/ArrangeEnums.java
@@ -8,7 +8,18 @@ package com.example.model.enums;
  */
 public enum ArrangeEnums {
 	
-	arr_DZL_WDJ ("scenario/御魂整理/待整理/未点击"),
+	arrange_DZL_WDJ ("scenario/御魂整理/待整理/未点击"),
+	arrange_GHYH("scenario/御魂整理/更换御魂"),
+	arrange_ZL_WDJ("scenario/御魂整理/整理/未点击"),
+	arrange_FA_WDJ("scenario/御魂整理/方案/未点击"),
+	arrange_FAXQH_WDJ("scenario/御魂整理/方案下强化/未点击"),
+	arrange_FASDTS_WDJ("scenario/御魂整理/方案速度提升/未点击"),
+	arrange_PX("scenario/御魂整理/排序"),
+	arrange_DZL_YDJ ("scenario/御魂整理/待整理/已点击"),
+	arrange_JRQH ("scenario/御魂整理/进入强化"),
+	arrange_SXQJG ("scenario/御魂整理/四星青吉鬼"),
+	arrange_QH ("scenario/御魂整理/强化"),
+	arrange_DJTS ("scenario/御魂整理/等级提升"),
 	;
 	
 	ArrangeEnums(String value) {
--- java/scenario/scenario/src/test/java/com/example/service/FightTest.java
+++ java/scenario/scenario/src/test/java/com/example/service/FightTest.java
@@ -1,9 +1,11 @@
 package com.example.service;
 
+import com.example.model.enums.ArrangeEnums;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.junit.Test;
 
+import java.awt.*;
 import java.io.IOException;
 
 /**
@@ -19,12 +21,20 @@ public class FightTest {
 	
 	//御魂强化-提升速度
 	@Test
-	public void FightTest1 () throws IOException {
+	public void FightTest1 () throws IOException, InterruptedException, AWTException {
 		logger.info ("御魂整理开始");
 		if (b1) {
 		      //循环强化速度御魂
 		      for(int i=1;i<=1;i++){
-				  
+				  logger.info ("进入更换御魂");
+				  ImageService.imagesClickBack (ArrangeEnums.arrange_GHYH.getValue ());
+				  Thread.sleep (1000);
+			      logger.info ("进入整理");
+			      ImageService.imagesClickBack (ArrangeEnums.arrange_ZL_WDJ.getValue ());
+			      Thread.sleep (1000);
+			      logger.info ("进入方案");
+			      ImageService.imagesClickBack (ArrangeEnums.arrange_FA_WDJ.getValue ());
+			      Thread.sleep (1000);
 		      }
 		}
 	}
