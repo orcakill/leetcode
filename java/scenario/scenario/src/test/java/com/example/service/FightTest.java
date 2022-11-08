52b18c69b300ecddc2cc8f3e8eb49571282e9f64
orcakill <orcakill@dingtalk.com>
2022-11-09 01:14:33 +0800

    feat(java-游戏脚本-御魂整理测试类): java-游戏脚本-御魂整理测试类
    java-游戏脚本-御魂整理测试类

--- java/scenario/scenario/src/test/java/com/example/service/FightTest.java
+++ java/scenario/scenario/src/test/java/com/example/service/FightTest.java
@@ -23,9 +23,17 @@ public class FightTest {
 	@Test
 	public void FightTest1 () throws IOException, InterruptedException, AWTException {
 		logger.info ("御魂整理开始");
+		//御魂强化属性
+		String soulSubduingEnhancementAttribute=null;
+		//强化状态
+		boolean reinforcementState=true;
+		//强化次数
+		int     strengtheningTimes=1;
+		//消耗材料数量
+		int     quantityOfConsumableMaterials=0;
 		if (b1) {
 		      //循环强化速度御魂
-		      for(int i=1;i<=1;i++){
+		      for(int i=1;i<=2;i++){
 				  logger.info ("进入更换御魂");
 				  ImageService.imagesClickBack (ArrangeEnums.arrange_GHYH.getValue ());
 				  Thread.sleep (1000);
@@ -35,6 +43,29 @@ public class FightTest {
 			      logger.info ("进入方案");
 			      ImageService.imagesClickBack (ArrangeEnums.arrange_FA_WDJ.getValue ());
 			      Thread.sleep (1000);
+			      logger.info ("进入方案下强化");
+			      ImageService.imagesClickBack (ArrangeEnums.arrange_FAXQH_WDJ.getValue ());
+			      Thread.sleep (1000);
+			      logger.info ("进入方案速度提升");
+			      ImageService.imagesClickBack (ArrangeEnums.arrange_FASDTS_WDJ.getValue ());
+			      Thread.sleep (1000);
+			      logger.info ("排序");
+			      ImageService.imagesClickBack (ArrangeEnums.arrange_PX.getValue ());
+			      Thread.sleep (1000);
+			      logger.info ("点击左上角御魂");
+			      ImageService.imagesClickBack (ArrangeEnums.arrange_ZSJYH.getValue ());
+				  Thread.sleep (1000);
+			      logger.info ("进入强化");
+			      ImageService.imagesClickBack (ArrangeEnums.arrange_JRQH.getValue ());
+			      Thread.sleep (1000);
+				  while (!reinforcementState){
+					  if(strengtheningTimes==1){
+						 
+					  }
+					  logger.info ("结束强化");
+					  reinforcementState=false;
+				  }
+				  System.exit (0);
 		      }
 		}
 	}
