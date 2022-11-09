88a8c2007dc42dac4d0487550443c1b1a9d720e2
orcakill <orcakill@dingtalk.com>
2022-11-10 00:12:27 +0800

    feat(java-游戏脚本-御魂整理测试类): java-游戏脚本-御魂整理测试类
    java-游戏脚本-御魂整理测试类

--- java/scenario/scenario/src/test/java/com/example/service/FightTest.java
+++ java/scenario/scenario/src/test/java/com/example/service/FightTest.java
@@ -55,7 +55,7 @@ public class FightTest {
 		strengthenResultSet.put ("防御加成",ArrangeEnums.arrange_YHQHSX_FYJC.getValue ());
 		if (b1) {
 		      //循环强化速度御魂
-		      for(int i=1;i<=2;i++){
+		      for(int i=1;i<=1;i++){
 				  //设置强化状态
 			      reinforcementState=true;
 				  logger.info ("进入更换御魂");
@@ -96,18 +96,18 @@ public class FightTest {
 						  quantityOfConsumableMaterials=4;
 					  }
 					  if(strengtheningTimes==3){
-						  logger.info ("第三次强化，强化+9,4个四星青吉鬼");
-						  quantityOfConsumableMaterials=4;
+						  logger.info ("第三次强化，强化+9,5个四星青吉鬼");
+						  quantityOfConsumableMaterials=5;
 					  }
 					  if(strengtheningTimes==4){
-						  logger.info ("第四次强化，强化+12,12个四星青吉鬼");
-						  quantityOfConsumableMaterials=12;
+						  logger.info ("第四次强化，强化+12,9个四星青吉鬼");
+						  quantityOfConsumableMaterials=9;
 					  }
 					  if(strengtheningTimes==5){
 						  logger.info ("第五次强化，强化+15,12个四星青吉鬼");
 						  quantityOfConsumableMaterials=12;
 					  }
-					  logger.info ("第strengtheningTimes:{}次,点击quantityOfConsumableMaterials:{}个四星青吉鬼",strengtheningTimes,quantityOfConsumableMaterials);
+					  logger.info ("第{}次,点击{}个四星青吉鬼",strengtheningTimes,quantityOfConsumableMaterials);
 					  for(int j=1;j<=quantityOfConsumableMaterials;j++){
 						  ImageService.imagesClickBack (ArrangeEnums.arrange_SXQJG.getValue ());
 						  Thread.sleep (1000);
@@ -116,7 +116,7 @@ public class FightTest {
 					  ImageService.imagesClickBack (ArrangeEnums.arrange_QH.getValue ());
 					  logger.info ("等级提升");
 					  levelPromotion=ImageService.imagesClickBackIsEmpty (ArrangeEnums.arrange_DJTS.getValue (),5);
-					  while (levelPromotion){
+					  while (!levelPromotion){
 						  Thread.sleep (1000);
 						  levelPromotion=ImageService.imagesClickBackIsEmpty (ArrangeEnums.arrange_DJTS.getValue (),5);
 					  }
@@ -127,42 +127,61 @@ public class FightTest {
 						  logger.info ("未找到御魂强化属性，程序退出");
 						  System.exit (0);
 					  }
-					  if(strengtheningTimes==1){
-						  logger.info ("第一次强化，记录强化属性，继续强化");
-						  soulSubduingEnhancementAttributeLast=soulSubduingEnhancementAttribute;
-						  logger.info ("确定");
-						  ImageService.imagesClickBack (ArrangeEnums.arrange_QD.getValue ());
-						  logger.info ("御魂强化次数+1");
-						  strengtheningTimes++;
+					  //如果强化属性为攻击、生命和防御，则直接弃置
+					  if((soulSubduingEnhancementAttribute.equals ("攻击")||soulSubduingEnhancementAttribute.equals ("生命")||
+					     soulSubduingEnhancementAttribute.equals ("防御"))&&strengtheningTimes <4){
+						  logger.info ("御魂强化属性为{},直接弃置",soulSubduingEnhancementAttribute);
+						  logger.info ("弃置");
+						  ImageService.imagesClickBack(ArrangeEnums.arrange_QZ.getValue ());
 						  Thread.sleep (1000);
+						  logger.info ("确定强化结果，本轮强化结束");
+						  ImageService.imagesClickBack(ArrangeEnums.arrange_QD.getValue ());
+						  Thread.sleep (1000);
+						  logger.info ("结束强化，强化状态置为false,强化次数置为1");
+						  reinforcementState=false;
+						  strengtheningTimes=1;
 					  }
 					  else {
-						  if(soulSubduingEnhancementAttribute.equals (soulSubduingEnhancementAttributeLast)||strengtheningTimes>=4){
-							  logger.info ("御魂强化属性和上一次相同，或者御魂强化次数大于等4次");
-							  logger.info ("确定强化结果，本轮强化结束");
-							  ImageService.imagesClickBack(ArrangeEnums.arrange_QD.getValue ());
-							  Thread.sleep (1000);
-							  if(strengtheningTimes==5){
-								  logger.info ("强化满级,本次强化结束");
-								  logger.info ("结束强化，强化状态置为false,强化次数置为1");
-								  reinforcementState=false;
-								  strengtheningTimes=1;
-							  }
+						  if (strengtheningTimes == 1) {
+							  logger.info ("第一次强化，记录强化属性，继续强化");
+							  soulSubduingEnhancementAttributeLast = soulSubduingEnhancementAttribute;
+							  logger.info ("确定");
+							  ImageService.imagesClickBack (ArrangeEnums.arrange_QD.getValue ());
 							  logger.info ("御魂强化次数+1");
 							  strengtheningTimes++;
-							  logger.info ("强化未满级，进入下一轮");
+							  Thread.sleep (1000);
 						  }
-						  else{
-							  logger.info ("御魂强化属性和上一次不同,且强化次数小于4次");
-							  logger.info ("弃置");
-							  ImageService.imagesClickBack(ArrangeEnums.arrange_QZ.getValue ());
-							  Thread.sleep (1000);
-							  logger.info ("确定强化结果，本轮强化结束");
-							  ImageService.imagesClickBack(ArrangeEnums.arrange_QD.getValue ());
-							  Thread.sleep (1000);
-							  logger.info ("结束强化，强化状态置为false,强化次数置为1");
-							  reinforcementState=false;
-							  strengtheningTimes=1;
+						  else {
+							  if (soulSubduingEnhancementAttribute.equals (soulSubduingEnhancementAttributeLast) || strengtheningTimes >= 4) {
+								  logger.info ("御魂强化属性和上一次相同，或者御魂强化次数大于等4次");
+								  logger.info ("确定强化结果，本轮强化结束");
+								  ImageService.imagesClickBack (ArrangeEnums.arrange_QD.getValue ());
+								  Thread.sleep (1000);
+								  if (strengtheningTimes == 5) {
+									  logger.info ("强化满级,本次强化结束");
+									  logger.info ("结束强化，强化状态置为false,强化次数置为1");
+									  reinforcementState = false;
+									  strengtheningTimes = 1;
+								  }
+								  else{
+									  logger.info ("强化未满级，进入下一轮");
+								  }
+								  logger.info ("御魂强化次数+1");
+								  strengtheningTimes++;
+								  
+							  }
+							  else {
+								  logger.info ("御魂强化属性和上一次不同,且强化次数小于4次");
+								  logger.info ("弃置");
+								  ImageService.imagesClickBack (ArrangeEnums.arrange_QZ.getValue ());
+								  Thread.sleep (1000);
+								  logger.info ("确定强化结果，本轮强化结束");
+								  ImageService.imagesClickBack (ArrangeEnums.arrange_QD.getValue ());
+								  Thread.sleep (1000);
+								  logger.info ("结束强化，强化状态置为false,强化次数置为1");
+								  reinforcementState = false;
+								  strengtheningTimes = 1;
+							  }
 						  }
 					  }
 				  }
