4f3e1315cfc3ff48b19bfe972d9f62fd85a1f76e
orcakill <orcakill@dingtalk.com>
2023-01-11 18:13:49 +0800

    fix(java-opencv特征匹配): java-opencv特征匹配
    java-opencv特征匹配

--- java/scenario/scenario/src/test/java/com/example/test/MultithreadingTest.java
+++ java/scenario/scenario/src/test/java/com/example/test/MultithreadingTest.java
@@ -1,12 +1,31 @@
 package com.example.test;
 
+import com.example.model.entity.BufferedImagePO;
+import com.example.model.entity.PictureIdentifyWorkPO;
+import com.example.util.ImagesOpenCVSIFT;
+import com.example.util.RandomUtil;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.junit.Test;
+import org.opencv.calib3d.Calib3d;
+import org.opencv.core.*;
+import org.opencv.features2d.DescriptorMatcher;
+import org.opencv.features2d.Features2d;
+import org.opencv.features2d.SIFT;
+import org.opencv.imgcodecs.Imgcodecs;
+import org.opencv.imgproc.Imgproc;
 
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
 import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
 import java.util.concurrent.*;
+import java.util.stream.Collectors;
+
+import static com.example.util.ImagesOpenCVSIFT.getMatify;
 
 /**
  * @author orcakill
@@ -18,7 +37,6 @@ import java.util.concurrent.*;
 public class MultithreadingTest {
 	
 	private static final Logger logger = LogManager.getLogger ("MultithreadingTest");
-
 	
 	@Test
 	public void test1 () {
@@ -60,61 +78,22 @@ public class MultithreadingTest {
 		for (int i = 0; i < 100; i++) {
 			list.add (i);
 		}
-		ThreadPoolExecutor executor = (ThreadPoolExecutor) Executors.newCachedThreadPool();
+		ThreadPoolExecutor executor = (ThreadPoolExecutor) Executors.newCachedThreadPool ();
 		List<Future<Integer>> resultList = new ArrayList<> ();
-		for (int i = 0; i < list.size (); i ++) {
-			FactorialCalculator factorialCalculator = new FactorialCalculator (list.get (i));
+		for (Integer integer : list) {
+			FactorialCalculator factorialCalculator = new FactorialCalculator (integer);
 			Future<Integer> res = executor.submit (factorialCalculator);
-			resultList.add(res);
+			resultList.add (res);
 		}
 		do {
-			for (int i = 0; i < resultList.size(); i++) {
-				logger.info(resultList.get (i).get ());
+			for (Future<Integer> integerFuture : resultList) {
+				logger.info (integerFuture.get ());
 			}
-		} while (executor.getCompletedTaskCount() < resultList.size());
+		} while (executor.getCompletedTaskCount () < resultList.size ());
 		executor.shutdownNow ();
 		long end = System.currentTimeMillis ();
 		logger.info ("doParallelStream cost:" + (end - start) / 1000);
 		
-	}
-	
-	
-	private static String execute (int i, int val, CountDownLatch countDownLatch) {
-		try {
-			Thread.sleep (1000);
-		} catch (InterruptedException e) {
-			throw new RuntimeException (e);
-		}
-		logger.info (val);
-		if (val == 6) {
-			countDownLatch.countDown ();
-			return String.valueOf (val);
-		}
-		return null;
-	}
-	
-	@Test
-	public void test4 () throws ExecutionException, InterruptedException {
-		long start = System.currentTimeMillis ();
-		ExecutorService executor = Executors.newFixedThreadPool (5);
-		CountDownLatch countDownLatch = new CountDownLatch (1);
-		List<Future> futureList = new ArrayList<> ();
-		List<Integer> list = new ArrayList<> ();
-		for (int i = 0; i < 10; i++) {
-			list.add (i);
-		}
-		for (int i = 0; i < list.size (); i++) {
-			int finalI = i;
-			futureList.add (executor.submit (() -> execute (finalI, list.get (finalI), countDownLatch)));
-		}
-		countDownLatch.await ();
-		logger.info ("--------------------");
-		for (Future<?> future : futureList) {
-			future.cancel (true);
-		}
-		logger.info ("结果{}", futureList.get (0).get ());
-		long end = System.currentTimeMillis ();
-		logger.info ("总耗时 :{}秒", (end - start) / 1000);
 	}
 	
 	public static class FactorialCalculator implements Callable<Integer> {
@@ -130,4 +109,159 @@ public class MultithreadingTest {
 			return number;
 		}
 	}
+	
+	@Test
+	public void test4 () throws ExecutionException, InterruptedException {
+		long start = System.currentTimeMillis ();
+		ExecutorService executor = Executors.newFixedThreadPool (5);
+		CountDownLatch countDownLatch = new CountDownLatch (1);
+		List<Future> futureList = new ArrayList<> ();
+		List<Integer> list = new ArrayList<> ();
+		for (int i = 0; i < 10; i++) {
+			list.add (i);
+		}
+		for (int i = 0; i < list.size (); i++) {
+			int finalI = i;
+			futureList.add (executor.submit (() -> execute (list.get (finalI), countDownLatch)));
+		}
+		countDownLatch.await ();
+		logger.info ("--------------------");
+		for (Future<?> future : futureList) {
+			future.cancel (true);
+		}
+		for(int i=0;i<futureList.size ();i++){
+			if(futureList.get (i).get ()!=null){
+				logger.info ("结果{}",futureList.get (i).get ());
+				break;
+			}
+		}
+		long end = System.currentTimeMillis ();
+		logger.info ("总耗时 :{}秒", (end - start) / 1000);
+	}
+	
+	private static  Integer execute (int val, CountDownLatch countDownLatch) {
+		try {
+			Thread.sleep (1000);
+		} catch (InterruptedException e) {
+			throw new RuntimeException (e);
+		}
+		logger.info (val);
+		if (val == 6) {
+			countDownLatch.countDown ();
+			return val;
+		}
+		return null;
+	}
+	
+	@Test
+	public void test5 () throws ExecutionException, InterruptedException {
+		long start = System.currentTimeMillis ();
+		ExecutorService executor = Executors.newFixedThreadPool (5);
+		CountDownLatch countDownLatch = new CountDownLatch (1);
+		List<Future> futureList = new ArrayList<> ();
+		List<Integer> list = new ArrayList<> ();
+		for (int i = 0; i < 10; i++) {
+			list.add (i);
+		}
+		for (Integer integer : list) {
+			FactorialCalculator1 factorialCalculator1 = new FactorialCalculator1 (integer, countDownLatch);
+			futureList.add (executor.submit (factorialCalculator1));
+		}
+		countDownLatch.await ();
+		logger.info ("--------------------");
+		for (Future<?> future : futureList) {
+			future.cancel (true);
+		}
+		for(int i=0;i<futureList.size ();i++){
+			if(futureList.get (i).get ()!=null){
+				logger.info ("结果{}",futureList.get (i).get ());
+				break;
+			}
+		}
+		long end = System.currentTimeMillis ();
+		logger.info ("总耗时 :{}秒", (end - start) / 1000);
+	}
+	
+	public static class FactorialCalculator1 implements Callable<Integer> {
+		
+		private final Integer number;
+		
+		private final CountDownLatch countDownLatch;
+		
+		public FactorialCalculator1 (Integer number, CountDownLatch countDownLatch) {
+			this.number = number;
+			this.countDownLatch = countDownLatch;
+		}
+		
+		public Integer call () {
+			try {
+				Thread.sleep (1000);
+			} catch (InterruptedException e) {
+				throw new RuntimeException (e);
+			}
+			logger.info (number);
+			if (number == 4) {
+				countDownLatch.countDown ();
+				return number;
+			}
+			return null;
+		}
+	}
+	public  void  test() throws IOException {
+		long start = System.currentTimeMillis ();
+		String str_src = "D:/a.png";
+		List<BufferedImagePO> ImagesData = ImagesOpenCVSIFT.readFiles ("scenario/阴阳寮/阴阳寮图标");
+		File file_src = new File (str_src);
+		BufferedImage bufferedImageSrc = ImageIO.read (file_src);
+		long end = System.currentTimeMillis ();
+		logger.info ("总耗时 :{}秒", (end - start) / 1000);
+	}
+	
+	@Test
+	public  void  test7() throws IOException {
+		long start = System.currentTimeMillis ();
+		ExecutorService executor = Executors.newFixedThreadPool (5);
+		CountDownLatch countDownLatch = new CountDownLatch (1);
+		List<Future> futureList = new ArrayList<> ();
+		System.loadLibrary (Core.NATIVE_LIBRARY_NAME);
+		String str_src = "D:/a.png";
+		List<BufferedImagePO> ImagesData = ImagesOpenCVSIFT.readFiles ("scenario/阴阳寮/阴阳寮图标");
+		File file_src = new File (str_src);
+		BufferedImage bufferedImageSrc = ImageIO.read (file_src);
+	}
+	
+	public static class FactorialCalculator2 implements Callable<PictureIdentifyWorkPO> {
+		private final BufferedImage originalImageB;
+		
+		private final BufferedImage templateImageB;
+		
+		private final Double coefficient;
+		
+		private final Boolean printOrNot;
+		private final int characteristicPoint;
+		private final  CountDownLatch countDownLatch;
+		
+		public FactorialCalculator2 (BufferedImage originalImageB, BufferedImage templateImageB, Double coefficient,
+		                             Boolean printOrNot, int characteristicPoint,CountDownLatch countDownLatch) {
+			this.originalImageB = originalImageB;
+			this.templateImageB = templateImageB;
+			this.coefficient = coefficient;
+			this.printOrNot = printOrNot;
+			this.characteristicPoint = characteristicPoint;
+			this.countDownLatch=countDownLatch;
+		}
+	
+		
+		public PictureIdentifyWorkPO call () {
+			PictureIdentifyWorkPO pictureIdentifyWorkPO=ImagesOpenCVSIFT.matchImage (originalImageB,templateImageB,coefficient,
+			                                                                         printOrNot,characteristicPoint);
+			if (pictureIdentifyWorkPO.getX ()!=null&&pictureIdentifyWorkPO.getY ()!=null) {
+				countDownLatch.countDown ();
+				return pictureIdentifyWorkPO;
+			}
+			return pictureIdentifyWorkPO;
+		}
+		
+	}
+
 }
