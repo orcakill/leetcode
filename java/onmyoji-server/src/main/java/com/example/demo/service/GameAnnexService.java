6e9bbf2ffac868b419dd769cff722e15a2c3494d
orcakill <orcakill@dingtalk.com>
2023-01-31 01:02:45 +0800

    feat(java-onmyoji：mybatisx测试完成): java-onmyoji：mybatisx测试完成
    java-onmyoji：mybatisx测试完成

---
+++ java/onmyoji-server/src/main/java/com/example/demo/service/GameAnnexService.java
@@ -0,0 +1,13 @@
+package com.example.demo.service;
+
+import com.example.demo.model.entity.GameAnnexPO;
+import com.baomidou.mybatisplus.extension.service.IService;
+
+/**
+* @author orca
+* @description 针对表【game_annex(游戏附录信息)】的数据库操作Service
+* @createDate 2023-01-30 21:25:19
+*/
+public interface GameAnnexService extends IService<GameAnnexPO> {
+
+}
--- java/onmyoji-server/src/main/java/com/example/demo/service/impl/GameAnnexServiceImpl.java
+++ java/onmyoji-server/src/main/java/com/example/demo/service/impl/GameAnnexServiceImpl.java
@@ -1,18 +1,19 @@
 package com.example.demo.service.impl;
 
-import com.baomidou.mybatisplus.extension.service.IService;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import com.example.demo.model.entity.GameAnnexPO;
+import com.example.demo.service.GameAnnexService;
 import com.example.demo.mapper.GameAnnexMapper;
 import org.springframework.stereotype.Service;
 
 /**
-* @author Administrator
+* @author orca
 * @description 针对表【game_annex(游戏附录信息)】的数据库操作Service实现
-* @createDate 2023-01-30 14:29:06
+* @createDate 2023-01-30 21:25:19
 */
 @Service
-public class GameAnnexServiceImpl extends ServiceImpl<GameAnnexMapper, GameAnnex>
-    implements IService<GameAnnex> {
+public class GameAnnexServiceImpl extends ServiceImpl<GameAnnexMapper, GameAnnexPO>
+    implements GameAnnexService{
 
 }
 
