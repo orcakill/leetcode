f150d01c96fae6ae7e4b1a3c18bbef362bb3d95a
orcakill <orcakill@dingtalk.com>
2023-02-02 00:37:36 +0800

    feat(java-onmyoji：图像算法优化效率): java-onmyoji：图像算法优化效率
    java-onmyoji：图像算法优化效率

--- java/onmyoji-server/src/test/java/com/example/demo/service/ImageServiceTest.java
+++ java/onmyoji-server/src/test/java/com/example/demo/service/ImageServiceTest.java
@@ -8,20 +8,19 @@ import org.springframework.boot.test.context.SpringBootTest;
 
 import javax.imageio.ImageIO;
 import java.awt.*;
-import java.awt.image.BufferedImage;
 import java.io.File;
+import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.ArrayList;
-import java.io.IOException;
-import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.TreeMap;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
-import static com.example.demo.model.enums.GameEnum.*;
+import static com.example.demo.model.enums.GameEnum.home_TS;
 
 /**
  * @Classname ImageServiceTest
@@ -76,20 +75,38 @@ class ImageServiceTest {
 		System.setProperty ("java.awt.headless", "false");
 		System.loadLibrary (Core.NATIVE_LIBRARY_NAME);
 		//Map<String,List<BufferedImagePO>> map=new HashMap<String,List<BufferedImagePO>>();
-		test ("D:\\project\\leetcode\\java\\onmyoji-server\\src\\main" +
-		                                            "\\resources\\static\\scenario");
+		String path="D:\\study\\Project\\leetcode\\java\\onmyoji-server\\src\\main" +
+		            "\\resources\\static\\scenario";
+		Map<String, List<BufferedImagePO>> map =test (path);
+		log.info (map.size ());
 		log.info ("测试结束");
 		long b=System.currentTimeMillis ();
 		log.info ("用时{}毫秒",b-a);
 	}
 	
-	private static void test(String path) throws IOException {
+	private static Map<String, List<BufferedImagePO>> test(String path) throws IOException {
+		List<File> files =new ArrayList<> ();
 		try (Stream<Path> paths = Files.walk (Paths.get (path))){
-			List<File> files =
-					paths.filter (Files::isRegularFile).map(l -> new File (l.toString ())).collect (Collectors.toList ());
+			files = paths.filter (Files::isRegularFile).map(l -> new File (l.toString ())).collect (Collectors.toList ());
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
-		
+		//创建map
+		Map<String, List<BufferedImagePO>> result = new TreeMap<> ();
+		//遍历所有信息，添加map
+		for (File file : files) {
+			String name=file.getParent ().replace("D:\\study\\Project\\leetcode\\java\\onmyoji-server\\src\\main" +
+			                                    "\\resources\\static\\","");
+			BufferedImagePO bufferedImagePO=new BufferedImagePO (1,name,ImageIO.read (file));
+			if (result.containsKey(name)) { //包含相同编号，
+				result.get(name).add(bufferedImagePO); //相同编号追加到集合中
+			}
+			else { //每组中只有一个的
+				ArrayList<BufferedImagePO> list = new ArrayList<>();
+				list.add(bufferedImagePO);
+				result.put(name, list);//直接添加到集合中
+			}
+		}
+		return result;
 	}
 }
