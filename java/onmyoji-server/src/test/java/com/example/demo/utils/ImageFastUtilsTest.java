a91bb4b1890f2c80b03ffbcc2f860368855753f2
orcakill <orcakill@dingtalk.com>
2023-03-03 01:58:17 +0800

    chore(java  onmyoji-server：orb算法不精确，确定放弃): orb算法不精确，确定放弃
    orb算法不精确，确定放弃

--- java/onmyoji-server/src/test/java/com/example/demo/utils/ImageFastUtilsTest.java
+++ java/onmyoji-server/src/test/java/com/example/demo/utils/ImageFastUtilsTest.java
@@ -20,9 +20,11 @@ import org.springframework.boot.test.context.SpringBootTest;
 import javax.imageio.ImageIO;
 import java.awt.*;
 import java.awt.image.BufferedImage;
+import java.io.ByteArrayOutputStream;
 import java.io.File;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.LinkedList;
 import java.util.List;
 
@@ -83,7 +85,7 @@ public class ImageFastUtilsTest {
 		// C++: static Ptr_ORB cv::ORB::create(int nfeatures = 500, float scaleFactor = 1.2f, int nlevels = 8,
 		// int edgeThreshold = 31, int firstLevel = 0, int WTA_K = 2, ORB_ScoreType scoreType = ORB::HARRIS_SCORE,
 		// int patchSize = 31, int fastThreshold = 20)
-		ORB sift = ORB.create (1000, 1.2f, 8, 31, 0, 2, ORB.HARRIS_SCORE, 3, 8);
+		ORB sift = ORB.create (800, 1.2f, 8, 31, 0, 2, ORB.FAST_SCORE, 3, 10);
 		//ORB sift = ORB.create ();
 		Mat templateImage;
 		MatOfKeyPoint templateKeyPoints = new MatOfKeyPoint ();
@@ -96,28 +98,20 @@ public class ImageFastUtilsTest {
 		long startTime = System.currentTimeMillis ();
 		//来源图片
 		//BufferedImage Window = ScreenshotUtils.screenshotBack ("夜神模拟器");
-		String str_tem = "D:/b.jpg";
+		String str_tem = "D:/a.jpg";
 		File file_tem = new File (str_tem);
 		BufferedImage Window = ImageIO.read (file_tem);
 		log.info (System.currentTimeMillis () - startTime);
 		log.info ("图像识别");
 		Mat originalImage = getMat (Window);
-		//Imgcodecs.imwrite ("D:\\b.jpg",originalImage );
+		//Imgcodecs.imwrite ("D:\\a.jpg", originalImage);
 		log.info (System.currentTimeMillis () - startTime);
 		sift.detect (originalImage, originalKeyPoints);
 		sift.compute (originalImage, originalKeyPoints, resO);
 		Mat mat1 =new Mat ();
 		Features2d.drawKeypoints (originalImage, originalKeyPoints, mat1, new Scalar (0, 0, 255),
 		                          Features2d.DrawMatchesFlags_DRAW_RICH_KEYPOINTS);
-		
-		File file_resO = new File ("D:\\resO.jpg");
-		if (file_resO.exists ()) {
-			boolean deleteNot = file_resO.delete ();
-			if (deleteNot) {
-				log.info ("先删除");
-			}
-		}
-		Imgcodecs.imwrite ("D:\\rea0.jpg", mat1);
+		Imgcodecs.imwrite ("D:\\mat1.jpg", mat1);
 		log.info (System.currentTimeMillis () - startTime);
 		for (PictureCollectionPO imagesDatum : pictureCollectionPOList) {
 			List<MatOfDMatch> matches = new LinkedList<> ();
@@ -126,20 +120,27 @@ public class ImageFastUtilsTest {
 			//获取模板图的特征点
 			sift.detect (templateImage, templateKeyPoints);
 			sift.compute (templateImage, templateKeyPoints, resT);
-			DescriptorMatcher descriptorMatcher = DescriptorMatcher.create (
-					DescriptorMatcher.FLANNBASED);
+			DescriptorMatcher descriptorMatcher = DescriptorMatcher.create (DescriptorMatcher.FLANNBASED);
 			resO.convertTo (resO,CvType.CV_32F,1/255.0);
 			resT.convertTo (resT,CvType.CV_32F, 1/255.0);
-			//knnMatch方法的作用就是在给定特征描述集合中寻找最佳匹配
-			//使用KNN-matching算法，令K=2，则每个match得到两个最接近的descriptor，然后计算最接近距离和次接近距离之间的比值，当比值大于既定值时，才作为最终match。
+			////knnMatch方法的作用就是在给定特征描述集合中寻找最佳匹配
+			////使用KNN-matching算法，令K=2，则每个match得到两个最接近的descriptor，然后计算最接近距离和次接近距离之间的比值，当比值大于既定值时，才作为最终match。
 			descriptorMatcher.knnMatch (resT, resO, matches, 2);
 			//System.out.println ("计算匹配结果");
-			goodMatchList (matches, goodMatchesList, 0.7);
+			goodMatchList (matches, goodMatchesList,0.7);
 			matchesPointCount = goodMatchesList.size ();
 			//当匹配后的特征点大于等于 4 个，则认为模板图在原图中，该值可以自行调整
 			log.info ("{} {},特征点:{},预计特征点{}", imagesDatum.getImageNumber (),
 			          imagesDatum.getImageName (),
 			          matchesPointCount, 4);
+			MatOfDMatch goodMatches = new MatOfDMatch ();
+			goodMatches.fromList (goodMatchesList);
+			Mat matchOutput =
+					new Mat (originalImage.rows () * 2, originalImage.cols () * 2, Imgcodecs.IMREAD_COLOR);
+			Features2d.drawMatches (templateImage, templateKeyPoints, originalImage, originalKeyPoints,
+			                        goodMatches, matchOutput, new Scalar (0, 255, 0),
+			                        new Scalar (255, 0, 0), new MatOfByte (), 2);
+			Imgcodecs.imwrite ("D:\\Output.jpg", matchOutput);
 			if (matchesPointCount >= 4) {
 				List<KeyPoint> templateKeyPointList = templateKeyPoints.toList ();
 				List<KeyPoint> originalKeyPointList = originalKeyPoints.toList ();
@@ -155,7 +156,7 @@ public class ImageFastUtilsTest {
 				scnMatOfPoint2f.fromList (scenePoints);
 				//使用 findHomography 寻找匹配上的关键点的变换
 				Mat homography = Calib3d.findHomography (objMatOfPoint2f, scnMatOfPoint2f, Calib3d.RANSAC,
-				                                         3);
+				                                         2);
 				//透视变换(Perspective Transformation)是将图片投影到一个新的视平面(Viewing Plane)，也称作投影映射(Projective Mapping)。
 				Mat templateCorners = new Mat (4, 1, CvType.CV_32FC2);
 				Mat templateTransformResult = new Mat (4, 1, CvType.CV_32FC2);
@@ -181,31 +182,8 @@ public class ImageFastUtilsTest {
 						                              pictureIdentifyWorkPO.getY () - (templateImage.rows () / 2.0)),
 						                   new Point (pictureIdentifyWorkPO.getX () + (templateImage.cols () / 2.0),
 						                              pictureIdentifyWorkPO.getY () + (templateImage.rows () / 2.0)),
-						                   new Scalar (0, 255, 0));
-						MatOfDMatch goodMatches = new MatOfDMatch ();
-						goodMatches.fromList (goodMatchesList);
-						Mat matchOutput =
-								new Mat (originalImage.rows () * 2, originalImage.cols () * 2, Imgcodecs.IMREAD_COLOR);
-						Features2d.drawMatches (templateImage, templateKeyPoints, originalImage, originalKeyPoints,
-						                        goodMatches, matchOutput, new Scalar (0, 255, 0),
-						                        new Scalar (255, 0, 0), new MatOfByte (), 2);
-						File file_Match = new File ("D:\\match.jpg");
-						if (file_Match.exists ()) {
-							boolean deleteNot = file_Match.delete ();
-							if (deleteNot) {
-								log.info ("先删除");
-							}
-						}
+						                   new Scalar (255, 0, 0));
 						Imgcodecs.imwrite ("D:\\match.jpg", originalImage);
-						
-						File file_Output = new File ("D:\\Output.jpg");
-						if (file_Output.exists ()) {
-							boolean deleteNot = file_Output.delete ();
-							if (deleteNot) {
-								log.info ("先删除");
-							}
-						}
-						Imgcodecs.imwrite ("D:\\Output.jpg", matchOutput);
 					}
 					log.info ("目标坐标为:({}，{})", pictureIdentifyWorkPO.getX (),
 					          pictureIdentifyWorkPO.getY ());
@@ -215,6 +193,107 @@ public class ImageFastUtilsTest {
 					break;
 				}
 			}
+		}
+		log.info (System.currentTimeMillis () - startTime);
+		log.info ("鼠标点击");
+		//int mousePressTime = (int) (Math.random () * 500 + 100);
+		for (PictureIdentifyWorkPO mouseMessage : mouseMessages) {
+			// 解析鼠标坐标参数,低位为X轴,高位为Y轴坐标
+			X = new StringBuilder (Integer.toHexString ((int) (mouseMessage.getX () / bl)));
+			Y = new StringBuilder (Integer.toHexString ((int) (mouseMessage.getY () / bl)));
+			while (X.length () < 4) {
+				X.insert (0, "0");
+			}
+			while (Y.length () < 4) {
+				Y.insert (0, "0");
+			}
+			Integer in = Integer.valueOf (Y + X.toString (), 16);
+			WinDef.LPARAM lPARAM = new WinDef.LPARAM (in);
+			// 模拟计算鼠标按下的间隔并且按下鼠标
+			ScanningProcessUtils.User32.INSTANCE.PostMessage (hwnd, 513, new WinDef.WPARAM (513), lPARAM);
+			ScanningProcessUtils.User32.INSTANCE.PostMessage (hwnd, 514, new WinDef.WPARAM (514), lPARAM);
+		}
+		log.info ("测试结束");
+		log.info ("用时{}毫秒", System.currentTimeMillis () - startTime);
+	}
+	
+	@Test
+	void test2 () throws IOException, AWTException {
+		log.info ("测试开始");
+		System.setProperty ("java.awt.headless", "false");
+		System.loadLibrary (Core.NATIVE_LIBRARY_NAME);
+		//图片集
+		String path = FolderPathMap.folderPath ("图片总路径");
+		List<PictureCollectionPO> pictureCollectionPOList =
+				ReadFileUtils.readPictureCollectionPOList (path, explore_DTXGZD, "SIFT");
+		//声明 坐标列表
+		List<PictureIdentifyWorkPO> mouseMessages = new ArrayList<> ();
+		//声明 识别坐标
+		PictureIdentifyWorkPO pictureIdentifyWorkPO = new PictureIdentifyWorkPO ();
+		WinDef.HWND hwnd = User32.INSTANCE.FindWindow (null, "谷歌浏览器");
+		Double bl = ComputerScalingUtils.getScale ();
+		StringBuilder X;
+		StringBuilder Y;
+		log.info ("开始计时");
+		long startTime = System.currentTimeMillis ();
+		//来源图片
+		//BufferedImage Window = ScreenshotUtils.screenshotBack ("夜神模拟器");
+		String str_tem = "D:/a.jpg";
+		File file_tem = new File (str_tem);
+		BufferedImage Window = ImageIO.read (file_tem);
+		log.info (System.currentTimeMillis () - startTime);
+		log.info ("图像识别");
+		//Imgcodecs.imwrite ("D:\\a.jpg", originalImage);
+		log.info (System.currentTimeMillis () - startTime);
+		List<Double> doubleList = new ArrayList<> ();
+		//获取来源图片.将来源图片转为Mat格式
+		Mat g_src = BufferImageToMatUtils.bufImg2Mat (Window, BufferedImage.TYPE_3BYTE_BGR, CvType.CV_8UC3);
+		//处理目标图片
+		for (PictureCollectionPO pictureCollectionPO : pictureCollectionPOList) {
+			//将目标图片转换为Mat
+			ByteArrayOutputStream byteArrayOutputStream2 = new ByteArrayOutputStream ();
+			ImageIO.write (pictureCollectionPO.getImage (), "jpg", byteArrayOutputStream2);
+			byteArrayOutputStream2.flush ();
+			Mat g_tem = BufferImageToMatUtils.bufImg2Mat (pictureCollectionPO.getImage (),
+			                                              BufferedImage.TYPE_3BYTE_BGR,
+			                                              CvType.CV_8UC3);
+			assert g_src != null;
+			assert g_tem != null;
+			int result_rows = g_src.rows () - g_tem.rows () + 1;
+			int result_cols = g_src.cols () - g_tem.cols () + 1;
+			Mat g_result = new Mat (result_rows, result_cols, CvType.CV_32FC1);
+			Imgproc.matchTemplate (g_src, g_tem, g_result, Imgproc.TM_SQDIFF_NORMED); // 归一化相关系数匹配法
+			Core.normalize (g_result, g_result, 0, 1, Core.NORM_MINMAX, -1, new Mat ());
+			Core.MinMaxLocResult core_result = Core.minMaxLoc (g_result);
+			Point matchLocation = core_result.minLoc; // 此处使用maxLoc还是minLoc取决于使用的匹配算法
+			log.info ("匹配系数{}",core_result.minVal);
+			//将匹配系数填入结果集
+			if (core_result.minVal > 0) {
+				doubleList.add (core_result.minVal);
+			}
+			//判断匹配系数大于预期，则返回坐标
+			if (core_result.minVal <= 2E-11 ) {
+				if (true) {
+					log.info ("打印图片");
+					Imgproc.rectangle (g_src, matchLocation,
+					                   new Point (matchLocation.x + g_tem.cols (), matchLocation.y + g_tem.rows ()),
+					                   new Scalar (0, 0, 0, 0));
+					Imgcodecs.imwrite ("D:\\match.jpg", g_src);
+				}
+				pictureIdentifyWorkPO.setX ((int) (matchLocation.x + g_tem.cols () / 2));
+				pictureIdentifyWorkPO.setY ((int) (matchLocation.y + g_tem.rows () / 2));
+				log.info ("找到坐标了,({},{})", pictureIdentifyWorkPO.getX (), pictureIdentifyWorkPO.getY ());
+				log.info ("已识别的匹配系数:{}", core_result.minVal);
+				//坐标添加到返回参数中
+				mouseMessages.add (pictureIdentifyWorkPO);
+			}
+			//识别出3个坐标后跳出
+			if (mouseMessages.size () >= 1) {
+				break;
+			}
+		}
+		if (doubleList.size () > 0) {
+			log.info ("最小匹配系数为{}", Collections.min (doubleList));
 		}
 		log.info (System.currentTimeMillis () - startTime);
 		log.info ("鼠标点击");
