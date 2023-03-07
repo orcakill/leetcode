ae713878fc46089a12d7d8dee4ba5699ba2e83a6
orcakill <orcakill@dingtalk.com>
2023-03-08 01:47:45 +0800

    feat(java  onmyoji-server：项目执行逻辑提取公共参数): 项目执行逻辑提取公共参数
    项目执行逻辑提取公共参数

---
+++ java/onmyoji-server/src/main/java/com/example/demo/model/var/ProjectVar.java
@@ -0,0 +1,18 @@
+package com.example.demo.model.var;
+
+/**
+ * @Classname ProjectVar
+ * @Description TODO
+ * @Date 2023/3/8 1:44
+ * @Created by orcakill
+ */
+public class ProjectVar {
+	
+	public  static  final String  project_JYJC="寄养检查";
+	public  static  final String  project_YYLTP="阴阳寮突破";
+	public  static  final String  project_GRTP="个人突破";
+	public  static  final String  project_HSY="魂十一";
+	public  static  final String  project_YYH="业原火";
+	public  static  final String  project_RLZY="日轮之陨";
+	
+}
