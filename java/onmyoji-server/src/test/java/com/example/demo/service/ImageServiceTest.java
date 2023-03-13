17ce1a9edfb7a919b82ac374e09fe86310920cae
orcakill <orcakill@dingtalk.com>
2023-03-14 00:54:51 +0800

    feat(java  onmyoji-server：探索优化): 探索优化
    探索优化，目前2个流程效率基本一致，30秒/个，后续添加结果分析，记录奖励结果，统计掉率

--- java/onmyoji-server/src/test/java/com/example/demo/service/ImageServiceTest.java
+++ java/onmyoji-server/src/test/java/com/example/demo/service/ImageServiceTest.java
@@ -10,8 +10,7 @@ import java.awt.*;
 import java.io.IOException;
 
 import static com.example.demo.model.param.ImageRecParam.paramSIFT;
-import static com.example.demo.model.var.CommVar.arrange_ZSJYH;
-import static com.example.demo.model.var.CommVar.home_TS;
+import static com.example.demo.model.var.CommVar.*;
 import static com.example.demo.service.ImageService.imagesBack;
 
 /**
@@ -30,7 +29,7 @@ class ImageServiceTest {
 		System.setProperty ("java.awt.headless", "false");
 		System.loadLibrary (Core.NATIVE_LIBRARY_NAME);
 		String process="夜神模拟器";
-		imagesBack (arrange_ZSJYH, paramSIFT (process));
+		imagesBack (explore_GDZR, paramSIFT (process, 0.7,5));
 		log.info ("测试结束");
 		log.info ("用时{}毫秒", System.currentTimeMillis () - startTime);
 	}
