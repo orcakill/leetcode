b44fbc83bb9bf357c7eba05b8fa210aab83a2865
orcakill <orcakill@dingtalk.com>
2023-04-13 00:02:58 +0800

    feat(java  onmyoji-server：地域鬼王+leet6): 地域鬼王
    地域鬼王

--- java/onmyoji-server/src/test/java/com/example/demo/utils/ImagesOpenCVSIFTUtilsTest.java
+++ java/onmyoji-server/src/test/java/com/example/demo/utils/ImagesOpenCVSIFTUtilsTest.java
@@ -6,16 +6,17 @@ import com.example.demo.model.map.FolderPathMap;
 import lombok.extern.log4j.Log4j2;
 import org.junit.jupiter.api.Test;
 import org.opencv.core.Core;
+import org.opencv.core.Mat;
+import org.opencv.imgcodecs.Imgcodecs;
 import org.springframework.boot.test.context.SpringBootTest;
 
-import javax.imageio.ImageIO;
 import java.awt.*;
 import java.awt.image.BufferedImage;
-import java.io.File;
 import java.io.IOException;
 import java.util.List;
 
-import static com.example.demo.model.var.CommVar.home_TS;
+import static com.example.demo.model.var.CommVar.ghost_WXZ;
+import static com.example.demo.utils.ImagesOpenCVSIFTUtils.getMat;
 
 /**
  * @Classname ImagesOpenCVSIFTUtilsTest
@@ -40,21 +41,21 @@ class ImagesOpenCVSIFTUtilsTest {
 		System.loadLibrary (Core.NATIVE_LIBRARY_NAME);
 		log.info ("测试开始");
 		//来源图片
-		//BufferedImage Window = ScreenshotUtils.screenshotBack ("夜神模拟器");
-		//Mat originalImage = getMat (Window);
-		//Imgcodecs.imwrite ("D:\\source.jpg",originalImage);
-		String str_tem = "D:/a.jpg";
-		File file_tem = new File (str_tem);
-		BufferedImage Window = ImageIO.read (file_tem);
+		BufferedImage Window = ScreenshotUtils.screenshotBack ("夜神模拟器");
+		Mat originalImage = getMat (Window);
+		Imgcodecs.imwrite ("D:\\source.jpg", originalImage);
+		//String str_tem = "D:/a.jpg";
+		//File file_tem = new File (str_tem);
+		//BufferedImage Window = ImageIO.read (file_tem);
 		//图片集
 		long startTime1 = System.currentTimeMillis ();
 		String path = FolderPathMap.folderPath ("图片总路径");
 		List<PictureCollectionPO> pictureCollectionPOList =
-				ReadFileUtils.readPictureCollectionPOList (path,home_TS, "SIFT");
+				ReadFileUtils.readPictureCollectionPOList (path,ghost_WXZ, "SIFT");
 		log.info ("图像录入用时{}毫秒", System.currentTimeMillis () - startTime1);
 		//屏幕截图和图片对比
 		List<PictureIdentifyWorkPO> mouseXY = ImagesOpenCVSIFTUtils.findPictureIdentifyWorkPOList (Window, pictureCollectionPOList, 0.7,
-		                                                               4,true);
+		                                                               5,true);
 		log.info ("图像识别，用时{}毫秒", System.currentTimeMillis () - startTime1);
 		assert mouseXY != null;
 		MouseClickUtils.mouseClickBack (mouseXY, "谷歌浏览器", true);
