8eccb3c8c0dcaf248d7bc03fb75320576bcd65c0
orcakill <orcakill@dingtalk.com>
2023-04-17 18:31:19 +0800

    chore(java onmyoji_server-leet6): leet6
    leet6

--- python/python/leetcode/leet6.py
+++ python/python/leetcode/leet6.py
@@ -1,13 +1,26 @@
 class Solution:
+    # 方法一  利用二维矩阵模拟
     @staticmethod
     def convert(s: str, numRows: int) -> str:
-        if numRows == 1:
+        # 字符长度n  待转行数 numRows
+        n, r = len(s), numRows
+        # 如果字符长度为1或者转换的字符长度大于待转的行数，直接返回字符s
+        if r == 1 or r >= n:
             return s
-
-        i = True
-
-        return s
+        t = r * 2 - 2
+        c = (n + t - 1) // t * (r - 1)
+        mat = [[''] * c for _ in range(r)]
+        x, y = 0, 0
+        for i, ch in enumerate(s):
+            mat[x][y] = ch
+            if i % t < r - 1:
+                x += 1
+            else:
+                x -= 1
+                y += 1
+        return ''.join(ch for row in mat for ch in row if ch)
 
 
 if __name__ == '__main__':
-    Solution.convert("PAYPALISHIRING", 3)
+    print(Solution.convert("PAYPALISHIRING", 3))
+
