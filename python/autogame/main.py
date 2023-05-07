e83032433da4c0c3957ce856073aaae11eca4652
orcakill <orcakill@dingtalk.com>
2023-05-08 00:51:23 +0800

    feat(beeware-测试框架是否正常): beeware-测试框架是否正常
    beeware-测试框架是否正常

--- python/autogame/main.py
+++ python/autogame/main.py
@@ -1,5 +1,10 @@
 from resources.my_logger import MyLogger
 
+from airtest.core.api import *
+
 if __name__ == '__main__':
     log = MyLogger().get_logger()
     log.info("脚本启动")
+    # 在auto_setup接口传入devices参数
+    auto_setup(__file__, logdir=True, devices=["android://"])
+    start_app("com.netease.onmyoji")
