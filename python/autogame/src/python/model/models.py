d6186509fc53115740471f7bb7011f0df4202219
orcakill <orcakill@dingtalk.com>
2023-05-10 01:55:57 +0800

    feat(python-autogame:测试sqlalchemy框架): python-autogame:测试sqlalchemy框架
    python-autogame:测试sqlalchemy框架

--- python/autogame/src/python/model/models.py
+++ python/autogame/src/python/model/models.py
@@ -1,25 +1,15 @@
-# coding=utf-8
+# coding: utf-8
 import configparser
-import datetime
-from pathlib import Path
 import uuid
+
+import guid as guid
 from sqlalchemy import Column, DateTime, Integer, String, create_engine
 from sqlalchemy.dialects.mysql import VARCHAR
-from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import declarative_base
 from sqlalchemy.orm import sessionmaker
-from src.python.utils.my_logger import MyLogger
-
-config = configparser.ConfigParser()
-project_path = Path.cwd().parent
-config_path = Path(project_path.joinpath("resources"), "config.ini")
-config.read(config_path, encoding="utf-8")
-engine = create_engine(config.get("DATABASE", "url"), echo=True)
-
-Session = sessionmaker(bind=engine)
-session = Session()
 
 Base = declarative_base()
-metadata = Base.metadata
+
 
 class GameThread(Base):
     __tablename__ = 'game_thread'
@@ -34,14 +24,16 @@ class GameThread(Base):
     update_time = Column(DateTime, comment='修改时间')
 
 
-def model_add(game_thread: GameThread):
-    session.add(game_thread)
-    session.commit()
-
-
 if __name__ == '__main__':
-    log = MyLogger().get_logger()
-    date01 = datetime.datetime.now()
-    log.info(str(uuid.uuid1))
-    gameThread = GameThread(id=str(uuid.uuid1))
-    model_add(gameThread)
+    guid = str(uuid.uuid4())
+    print(guid)
+    config = configparser.ConfigParser()
+    config.read("config.ini", encoding="utf-8")
+    url = config.get("database", "url")
+    engine = create_engine(url, echo=True)  # 实例化数据库连接
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    game_thread = GameThread(id=guid, ip="123.1.1.1")
+    session.add(game_thread)  # 添加数据
+    session.commit()  # 最后需要调用commit()方法提交事务。
+
