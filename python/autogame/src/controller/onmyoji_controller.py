685ce8e2c97bf61904ef668901f97f3e402b82a9
orcakill <orcakill@dingtalk.com>
2023-05-18 19:00:11 +0800

    ci(python-autogame:任务1编写): python-autogame:任务1编写
    python-autogame:任务1编写

--- python/autogame/src/controller/onmyoji_controller.py
+++ python/autogame/src/controller/onmyoji_controller.py
@@ -1,3 +1,15 @@
-def task(game_ip: str, game_type: int, game_round: int):
-    # 组织待执行项目
-    li = 1
+from src.utils.my_logger import my_logger as logger
+from src.model.param import *
+from src.model.enum import *
+
+
+def task(game_type: int, game_round: int):
+    project_list = []
+    if game_type == 1:
+        logger.info("大号：寄养+地域鬼王+组队挑战+阴阳寮突破+结界突破+御魂40次+好友增删+花合战奖励")
+        logger.info("协战号1、2、3：寄养+地域鬼王+组队挑战+结界突破协战2次+御魂协战14次+好友增删+花合战奖励")
+        logger.info("大号体验服：寄养+地域鬼王+组队挑战+阴阳寮突破+结界突破+御魂40次+好友增删+花合战奖励")
+        logger.info("小号1：寄养+地域鬼王+组队挑战+阴阳寮突破+结界突破+御魂40次+好友增删+花合战奖励")
+        project_list.append(ProjectParam(ProjectEnum.project_JYJC,))
+    projects = ProjectsParam(ProjectsEnum.project_1.value, game_round, [])
+    return projects
--- python/autogame/src/model/enum.py
+++ python/autogame/src/model/enum.py
@@ -1,7 +1,7 @@
 from enum import Enum
 
 
-class ProjectVar(Enum):
+class ProjectEnum(Enum):
     project_CSH = "当前状态初始化"
     project_JYJC = "寄养检查"
     project_YYLTP = "阴阳寮突破"
@@ -17,3 +17,7 @@ class ProjectVar(Enum):
     project_GRTS_FAST = "个人探索-极限速度"
     project_YL = "御灵"
     project_DYGW = "地域鬼王"
+
+
+class ProjectsEnum(Enum):
+    project_1 = "大号+小号1+协战号123+大号体验服"
