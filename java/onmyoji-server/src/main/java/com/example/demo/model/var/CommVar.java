5b45a8de4bca8e659957775ea2334c912d72745a
orcakill <orcakill@dingtalk.com>
2023-05-22 22:19:03 +0800

    feat(java  onmyoji-server：地域鬼王+leet6): 地域鬼王
    地域鬼王

--- java/onmyoji-server/src/main/java/com/example/demo/model/var/CommVar.java
+++ java/onmyoji-server/src/main/java/com/example/demo/model/var/CommVar.java
@@ -151,6 +151,7 @@ public class CommVar {
 	public static final String ghost_SCGW_NC="地域鬼王/收藏鬼王/鸟巢";
 	public static final String ghost_SCGW_SLSCJG="地域鬼王/收藏鬼王/少林寺藏经阁";
 	public static final String ghost_PTBZ="地域鬼王/普通标志";
+	public static final String ghost_JBZ="地域鬼王/极标志";
 	public static final String ghost_TZ="地域鬼王/挑战";
 	public static final String ghost_ZB="地域鬼王/准备";
 	
