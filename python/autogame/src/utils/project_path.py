05b76d497a4d7d2fd146c559b9096eb64511569e
orcakill <orcakill@dingtalk.com>
2023-06-16 18:25:05 +0800

    ci(python-autogame:当前状态初始化): 文件夹下图片存在识别
    文件夹下图片存在识别

--- python/autogame/src/utils/project_path.py
+++ python/autogame/src/utils/project_path.py
@@ -3,6 +3,10 @@ import configparser
 
 
 def get_project_path():
+    """
+    获取项目路径
+    :return: 项目根路径
+    """
     # 获取当前文件的绝对路径
     p_path = os.path.abspath(os.path.dirname(__file__))
     # 通过字符串截取方式截取，当前文件相对路径./src/util/StrUtil.py
@@ -14,5 +18,9 @@ def get_database_url():
     config_path = root_path + "src\\resources\\config.ini"
     config = configparser.ConfigParser()
     config.read(config_path, encoding="utf-8")
-    return  config.get("database", "url")
+    return config.get("database", "url")
 
+
+def get_onmyoji_image_path():
+    project_path = get_project_path()
+    return project_path + "src\\resources\\static\\onmyoji"
