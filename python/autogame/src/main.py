648aa605b86ca51a2aa06a886a93e2f07c1adf2b
orcakill <orcakill@dingtalk.com>
2023-06-28 18:22:39 +0800

    ci(python-autogame:当前状态初始化): 当前状态初始化
    当前状态初始化

--- python/autogame/src/main.py
+++ python/autogame/src/main.py
@@ -9,11 +9,12 @@ airtest_service = AirtestService()
 
 if __name__ == '__main__':
     logger.info("脚本启动")
-    # game_type = input("请输入一个脚本类型：")
-    # game_round = input("请输入一个脚本轮次：")
+    logger.info("1 大号、小号、协战号每日任务")
+    logger.info("2 协战号寄养、协战、领取奖励")
+    logger.info("null  默认设备 1 夜神模拟器")
+    game_type = input("请输入一个脚本类型：")
+    game_round = input("请输入一个脚本轮次：")
     game_device=input("请输入一个脚本设备：")
-    game_type = "1"
-    game_round = "1"
     # 0 每个节点都发送邮件 1 进程结束后发送邮件  2不发送邮件
     game_is_email = "0"
     logger.info("脚本类型 {},脚本轮次 {}", game_type, game_round)
