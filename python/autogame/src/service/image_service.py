d4fd6b31ba3e93a38f0605d7c524db638d1c7623
orcakill <orcakill@dingtalk.com>
2023-07-06 10:52:32 +0800

    ci(python-autogame:当前状态初始化): 拒接协战
    拒接协战-单线程

--- python/autogame/src/service/image_service.py
+++ python/autogame/src/service/image_service.py
@@ -7,7 +7,7 @@ import time
 
 from airtest.core.cv import Template
 
-from src.model.enum import Cvstrategy
+from src.model.enum import Cvstrategy, Onmyoji
 from src.service.airtest_service import AirtestService
 from src.utils.my_logger import my_logger as logger
 from src.utils.project_path import get_onmyoji_image_path
@@ -21,8 +21,8 @@ CVSTRATEGY = Cvstrategy.sift
 TIMEOUT = 20
 # 图像识别阈值
 THRESHOLD = 0.7
-# 图片识别次数（次）
-NUMBER = 1
+# 图片识别轮次
+REC_ROUND = 1
 # 图片识别间隔(秒）·
 INTERVAL = 1
 
@@ -30,36 +30,36 @@ INTERVAL = 1
 class ImageService:
     @staticmethod
     def exists(folder_path: str, cvstrategy: [] = CVSTRATEGY, timeout: int = TIMEOUT,
-               threshold: float = THRESHOLD, number: int = NUMBER, interval: int = INTERVAL) -> bool:
+               threshold: float = THRESHOLD, rec_round: int = REC_ROUND, interval: int = INTERVAL) -> bool:
         """
         根据文件夹名获取图片进行图像识别，判断图片是否存在
         :param folder_path: 图片文件夹路径
         :param cvstrategy: 图像识别算法
         :param timeout: 超时时间
         :param threshold: 图像识别阈值
-        :param number:   图片组识别次数
+        :param rec_round:   图片组识别次数
         :param interval:   图片识别间隔
         :return: bool
         """
-        return image_rec(folder_path, cvstrategy, timeout, "exists", threshold, number, interval)
+        return image_rec(folder_path, cvstrategy, timeout, "exists", threshold, rec_round, interval)
 
     @staticmethod
     def touch(folder_path: str, cvstrategy: [] = CVSTRATEGY, timeout: int = TIMEOUT,
-              threshold: float = THRESHOLD, number: int = NUMBER, interval: int = INTERVAL) -> bool:
+              threshold: float = THRESHOLD, rec_round: int = REC_ROUND, interval: int = INTERVAL) -> bool:
         """
         根据文件夹名获取图片进行图像识别，点击图片
         :param folder_path: 图片文件夹路径
         :param cvstrategy: 图像识别算法
         :param timeout: 超时时间
         :param threshold: 图像识别阈值
-        :param number:   图片组识别次数
+        :param rec_round:   图片组识别次数
         :param interval:   图片识别间隔
         :return: bool
         """
-        return image_rec(folder_path, cvstrategy, timeout, "touch", threshold, number, interval)
+        return image_rec(folder_path, cvstrategy, timeout, "touch", threshold, rec_round, interval)
 
 
-def image_rec(folder_path: str, cvstrategy: [], timeout: int, image_type: str, threshold: float, number: int,
+def image_rec(folder_path: str, cvstrategy: [], timeout: int, image_type: str, threshold: float, rec_round: int,
               interval: int) -> bool:
     """
     根据文件夹名获取图片进行图像识别，点击图片
@@ -68,35 +68,53 @@ def image_rec(folder_path: str, cvstrategy: [], timeout: int, image_type: str, t
     :param timeout: 单张图片超时时间
     :param image_type: 识别类型
     :param threshold: 图像识别阈值
-    :param number:   图片组识别次数
+    :param rec_round:   图片组识别次数
     :param interval:   图片识别间隔
     :return: bool
     """
-    folder_all_path = os.path.join(get_onmyoji_image_path(), folder_path)
-    for i in range(number):
-        for file_name in os.listdir(folder_all_path):
-            file_path = os.path.abspath(os.path.join(folder_all_path, file_name))
-            # 判断文件是否存在
-            if os.path.isfile(file_path):
-                # 判断文件是否是图片类型
-                file_ext = file_path.split('.')[-1].lower()
-                if file_ext in ['jpg', 'jpeg', 'png', 'gif', 'bmp']:
-                    # 图片类赋值
-                    template = Template(filename=file_path)
-                    # 根据识别类型，存在 或 存在并点击
-                    if image_type == "exists":
-                        # 判断图片是否存在
-                        if airtest_service.exists(template, cvstrategy, timeout, threshold):
-                            logger.debug("图片识别成功")
-                            return True
-                    elif image_type == "touch":
-                        # 判断图片是否存在并点击
-                        if airtest_service.touch(template, cvstrategy, timeout, threshold):
-                            logger.debug("图片识别点击成功")
-                            return True
-            else:
-                logger.debug("{}文件不存在", file_path)
-        logger.debug("第{}轮图片未识别", i+1)
+    for i in range(rec_round):
+        # 当前识别轮次
+        num = i + 1
+        if num >= 2:
+            # 识别轮次大于1时，判断是存在并点击悬赏封印
+            image_rec_one(Onmyoji.return_XSFYHSCH, cvstrategy, 1, "touch", threshold)
+        if not image_rec_one(folder_path, cvstrategy, timeout, image_type, threshold):
+            logger.debug("第{}轮图片未识别", num)
         # 等待 *秒
         time.sleep(interval)
+    return False
+
+
+def image_rec_one(folder_path: str, cvstrategy: [], timeout: int, image_type: str, threshold: float) -> bool:
+    """
+    根据文件夹名获取图片进行图像识别，点击图片
+    :param folder_path: 图片文件夹路径
+    :param cvstrategy: 图像识别算法
+    :param timeout: 单张图片超时时间
+    :param image_type: 识别类型
+    :param threshold: 图像识别阈值
+    :return: bool
+    """
+    folder_all_path = os.path.join(get_onmyoji_image_path(), folder_path)
+    for file_name in os.listdir(folder_all_path):
+        file_path = os.path.abspath(os.path.join(folder_all_path, file_name))
+        # 判断文件是否存在
+        if os.path.isfile(file_path):
+            # 判断文件是否是图片类型
+            file_ext = file_path.split('.')[-1].lower()
+            if file_ext in ['jpg', 'jpeg', 'png', 'gif', 'bmp']:
+                # 图片类赋值
+                template = Template(filename=file_path)
+                # 根据识别类型，存在 或 存在并点击
+                if image_type == "exists":
+                    # 判断图片是否存在
+                    if airtest_service.exists(template, cvstrategy, timeout, threshold):
+                        return True
+                elif image_type == "touch":
+                    # 判断图片是否存在并点击
+                    if airtest_service.touch(template, cvstrategy, timeout, threshold):
+                        return True
+        else:
+            logger.debug("{}文件不存在", file_path)
+
     return False
--- python/autogame/src/test/kwarg_test.py
+++ python/autogame/src/test/kwarg_test.py
@@ -3,15 +3,10 @@
 # @File    : kwarg_test.py
 # @Description : TODO
 
-def cs_test1(s: str, **kwargs):
-    print(s)
-    print(kwargs)
-    cs_test2(s, **kwargs)
-
-
-def cs_test2(s: str, **kwargs):
-    print(kwargs)
+def cs_test1():
+    for i in range(3):
+        print(i)
 
 
 if __name__ == '__main__':
-    cs_test1("1", tenp=1)
+    cs_test1()
