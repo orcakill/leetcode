5a6b86fa32ef9601019a9b37a82d49f8666ec1b4
orcakill <orcakill@dingtalk.com>
2023-08-16 00:11:25 +0800

    ci(python-autogame：逢魔+地域鬼王): 逢魔+地域鬼王
    逢魔（小号完成）+地域鬼王（小号完成）

--- python/autogame/src/test/test_onmyoji_service.py
+++ python/autogame/src/test/test_onmyoji_service.py
@@ -101,3 +101,20 @@ class TestOnmyojiService(TestCase):
         OnmyojiService.initialization(game_task)
         # 执行测试任务
         OnmyojiService.encounter_demons(game_task)
+
+    def test_ghost_king(self):
+        logger.debug("地域鬼王")
+        test_name, test_devices = '4', '0'
+        # 初始化测试任务信息
+        game_projects = GameProjects()
+        game_projects_relation = GameProjectsRelation()
+        game_account = select_game_account(test_name)
+        game_project = GameProject()
+        game_task = [game_projects, game_projects_relation, game_account, game_project]
+        logger.debug("开始测试-地域鬼王")
+        # 初始化设备信息
+        AirtestService.auto_setup(test_devices)
+        # 当前状态初始化
+        OnmyojiService.initialization(game_task)
+        # 执行测试任务
+        OnmyojiService.ghost_king(game_task)
