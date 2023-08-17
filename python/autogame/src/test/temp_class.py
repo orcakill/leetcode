a470304da22b18c28d5aea342d9e834cf7e01436
orcakill <orcakill@dingtalk.com>
2023-08-17 18:50:00 +0800

    ci(python-autogame:好友管理): 好友管理
    好友管理

---
+++ python/autogame/src/test/temp_class.py
@@ -0,0 +1,17 @@
+# @Time: 2023年08月17日 09:43
+# @Author: orcakill
+# @File: temp_class.py
+# @Description: 测试类方法自我调用
+class MyClass:
+    @staticmethod
+    def method1():
+        print(1)
+
+    @staticmethod
+    def method2():
+        MyClass.method1()  # 调用方法一
+        # 方法二的实现
+
+
+if __name__ == '__main__':
+    MyClass.method2()
