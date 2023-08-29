ce6cecbb3848e88af5e1b177df115c49a88c0f44
orcakill <orcakill@dingtalk.com>
2023-08-29 18:43:34 +0800

    ci(python-autogame:大号-夜神模拟器测试): 大号-夜神模拟器测试
    阴阳寮测试，lbs鬼王测试

--- python/autogame/src/test/test_activity_service.py
+++ python/autogame/src/test/test_activity_service.py
@@ -29,6 +29,36 @@ project_interrupt_flag = False
 class TestActivityService(TestCase):
 
     @staticmethod
+    def test_current_activity0():
+        """
+        当前活动,设备云手机
+        :return:
+        """
+        test_names = ['1', '2']
+        test_devices = '0'
+        TestActivityService.test_current_activity(test_names, test_devices)
+
+    @staticmethod
+    def test_current_activity1():
+        """
+        当前活动,设备夜神模拟器
+        :return:
+        """
+        test_names = ['1', '2']
+        test_devices = '1'
+        TestActivityService.test_current_activity(test_names, test_devices)
+
+    @staticmethod
+    def test_current_activity2():
+        """
+        当前活动,设备荣耀平板
+        :return:
+        """
+        test_names = ['1', '2']
+        test_devices = '2'
+        TestActivityService.test_current_activity(test_names, test_devices)
+
+    @staticmethod
     def assist_activity():
         global project_interrupt_flag
         logger.debug("开启拒接协战")
@@ -36,16 +66,16 @@ class TestActivityService(TestCase):
             time.sleep(30)
             image_service.touch(Onmyoji.comm_FH_XSFYHSCH, cvstrategy=Cvstrategy.default)
 
-    def test_current_activity0(self):
+    @staticmethod
+    def test_current_activity(test_names, test_devices):
         global project_interrupt_flag
         logger.debug("当前活动测试-云手机")
-        test_names = ['2', '3', '4', '5']
-        # test_names = ['3']
-        test_devices = '0'
+        test_names = test_names
+        test_devices = test_devices
         # 初始化设备信息
         image_service.auto_setup(test_devices)
         # 拒接协战
-        thread2 = threading.Thread(target=self.assist_activity, args=())
+        thread2 = threading.Thread(target=TestActivityService.assist_activity, args=())
         thread2.start()
         for i in range(len(test_names)):
             test_name = test_names[i]
@@ -66,16 +96,16 @@ class TestActivityService(TestCase):
             project_interrupt_flag = True
         thread2.join()
 
-    def test_current_activity1(self):
+    @staticmethod
+    def test_current_lbs():
         global project_interrupt_flag
-        logger.debug("当前活动测试-夜神模拟器")
-        # test_names = ['2', '3', '4', '5']
-        test_names = ['3']
+        logger.debug("LBS鬼王测试")
+        test_names = ['1']
         test_devices = '1'
         # 初始化设备信息
         image_service.auto_setup(test_devices)
         # 拒接协战
-        thread2 = threading.Thread(target=self.assist_activity, args=())
+        thread2 = threading.Thread(target=TestActivityService.assist_activity, args=())
         thread2.start()
         for i in range(len(test_names)):
             test_name = test_names[i]
@@ -85,39 +115,14 @@ class TestActivityService(TestCase):
             game_account = select_game_account(test_name)
             game_project = GameProject()
             game_task = [game_projects, game_projects_relation, game_account, game_project]
-            logger.debug("开始测试-当前活动-夜神模拟器")
+            logger.debug("开始测试-LBS鬼王")
+            OnmyojiService.return_home(game_task)
+            # 当前状态初始化
+            OnmyojiService.initialization(game_task)
             # 当前状态初始化
             OnmyojiService.initialization(game_task)
             # 执行测试任务
-            activity_service.current_activity(game_task)
-            logger.debug("{}测试完成", test_name)
-            project_interrupt_flag = True
-        thread2.join()
-
-    def test_current_activity2(self):
-        global project_interrupt_flag
-        logger.debug("当前活动测试-荣耀平板")
-        # test_names = ['2', '3', '4', '5']
-        test_names = ['4']
-        test_devices = '2'
-        # 初始化设备信息
-        image_service.auto_setup(test_devices)
-        # 拒接协战
-        thread2 = threading.Thread(target=self.assist_activity, args=())
-        thread2.start()
-        for i in range(len(test_names)):
-            test_name = test_names[i]
-            # 初始化测试任务信息
-            game_projects = GameProjects()
-            game_projects_relation = GameProjectsRelation()
-            game_account = select_game_account(test_name)
-            game_project = GameProject()
-            game_task = [game_projects, game_projects_relation, game_account, game_project]
-            logger.debug("开始测试-当前活动-荣耀平板")
-            # 当前状态初始化
-            OnmyojiService.initialization(game_task)
-            # 执行测试任务
-            activity_service.current_activity(game_task)
+            activity_service.current_lbs(game_task)
             logger.debug("{}测试完成", test_name)
             project_interrupt_flag = True
         thread2.join()
