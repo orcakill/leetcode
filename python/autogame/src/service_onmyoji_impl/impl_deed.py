7bfa885528dd5df9c4b19de8c3e03344f4ca3867
orcakill <orcakill@dingtalk.com>
2023-09-26 18:34:00 +0800

    feat(python-autogame:24项目全部完成，脚本3.0初步完成): 24项目全部完成，脚本3.0初步完成
    24项目全部完成，脚本3.0初步完成

--- python/autogame/src/service_onmyoji_impl/impl_deed.py
+++ python/autogame/src/service_onmyoji_impl/impl_deed.py
@@ -37,22 +37,31 @@ def deed_spirit(game_task: []):
         # 判断是否有契灵，无则使用鸣契石召唤5次并战斗，直至无鸣契石，之后探查50次，清理探查出
         logger.debug("当前契灵首页")
         logger.debug("鸣契石召唤，契灵战斗")
-        for i_stone in range(5):
-            logger.debug("第{}次召唤")
+        for i_stone in range(1,31):
+            logger.debug("鸣契石契灵战斗")
+            deed_fight()
+            logger.debug("第{}次召唤",i_stone)
             ImageService.touch(Onmyoji.deed_MQZH)
             logger.debug("镇墓兽")
             ImageService.touch(Onmyoji.deed_ZHZMS)
             logger.debug("召唤确认")
-            ImageService.touch(Onmyoji.deed_ZHQR)
-        logger.debug("鸣契石契灵战斗")
-        deed_fight()
+            is_confirm=ImageService.touch(Onmyoji.deed_ZHQR)
+            if not is_confirm:
+                logger.debug("可能地图已满契灵,或者无鸣契石")
+                break
         logger.debug("探查50次")
         for i_exploration in range(50):
             logger.debug("点击探查{}", i_exploration + 1)
             ImageService.touch(Onmyoji.deed_TC)
             logger.debug("等待战斗结果")
-            ComplexService.fight_end(Onmyoji.deed_ZDSL, Onmyoji.deed_ZDSB, Onmyoji.deed_ZCTZ,
+            is_result=ComplexService.fight_end(Onmyoji.deed_ZDSL, Onmyoji.deed_ZDSB, Onmyoji.deed_ZCTZ,
                                      Onmyoji.deed_TCTZ, Onmyoji.deed_TC, None, 60, 4)
+            if is_result in [Onmyoji.deed_ZDSB,Onmyoji.deed_ZCTZ]:
+                logger.debug("阵容问题，退出循环")
+                break
+            elif is_result in [Onmyoji.deed_TC]:
+                logger.debug("契灵已满")
+                break
         logger.debug("探查契灵战斗")
         deed_fight()
     logger.debug("确认返回首页")
@@ -66,7 +75,8 @@ def deed_fight():
     契灵战斗
     :return:
     """
-    for i in range(6):
+    is_unlock = False
+    for i in range(1, 50):
         logger.debug("判断当前是否存在契灵 1契灵地图 2 契灵界面")
         is_boss = ImageService.exists(Onmyoji.deed_QL)
         is_fight = ImageService.exists(Onmyoji.deed_QLTZ)
@@ -76,14 +86,22 @@ def deed_fight():
                 ImageService.touch(Onmyoji.deed_QL)
             if is_fight:
                 logger.debug("契灵结契界面")
+            if is_unlock or i == 1:
+                ImageService.touch(Onmyoji.deed_SDZR)
             logger.debug("点击契灵挑战")
-            is_fight = ImageService.touch(Onmyoji.deed_QLTZ)
-            if not is_fight:
+            ImageService.touch(Onmyoji.deed_QLTZ)
+            is_auto = ImageService.exists(Onmyoji.deed_ZDZD, timeouts=5)
+            if not is_auto:
                 ComplexService.refuse_reward()
                 logger.debug("重新点击契灵挑战")
                 ImageService.touch(Onmyoji.deed_QLTZ)
-            ComplexService.fight_end(Onmyoji.deed_ZDSL, Onmyoji.deed_ZDSL, Onmyoji.deed_ZCTZ, Onmyoji.deed_TCTZ,
+                logger.debug("点击准备")
+                is_unlock = ImageService.touch(Onmyoji.deed_ZB)
+            is_result=ComplexService.fight_end(Onmyoji.deed_ZDSL, Onmyoji.deed_ZDSL, Onmyoji.deed_ZCTZ, Onmyoji.deed_TCTZ,
                                      Onmyoji.deed_QLTZ, None, 300, 2)
+            if is_result in [Onmyoji.deed_ZDSB,Onmyoji.deed_ZCTZ]:
+                logger.debug("阵容问题，退出循环")
+                break
         else:
             logger.debug("不存在契灵")
             break
