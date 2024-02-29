03a2e64761281ed08485e09fd98aad7dd43c776f
orcakill <orcakill@dingtalk.com>
2024-02-29 18:06:26 +0800

    feat(python-autogame:测试调整): 测试调整
    测试调整

--- python/autogame/src/test/test_hwnd_service.py
+++ python/autogame/src/test/test_hwnd_service.py
@@ -7,16 +7,25 @@ from unittest import TestCase
 from src.service.hwnd_service import HwndService
 from src.utils.my_logger import my_logger as logger
 
+
 # 1.获取当前窗口的信息，进程名，类名，句柄 test_get_current_hwnd()
 # 2、根据句柄获取当前窗口的类名
 class TestHwndService(TestCase):
     # 1.获取当前窗口的信息，进程名，类名，句柄
     @staticmethod
     def test_get_current_hwnd():
+        """
+        获取当前窗口的信息，进程名，类名，句柄
+        :return:
+        """
         HwndService.get_current_hwnd()
 
     @staticmethod
     def test_get_hwnd_class_name():
-        hwnd=0
-        class_name=HwndService.get_hwnd_class_name(198902)
+        class_name = HwndService.get_hwnd_class_name(133388)
         logger.debug(class_name)
+
+    @staticmethod
+    def test_find_hwnd():
+        hwnd= HwndService.find_hwnd("Edit")
+        logger.debug(hwnd)
