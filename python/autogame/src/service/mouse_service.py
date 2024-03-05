f67ddc029bac714e4d57d6e2fb3830b757f5128e
orcakill <orcakill@dingtalk.com>
2024-03-05 19:43:52 +0800

    feat(python-autogame:测试调整): 测试调整
    测试调整

--- python/autogame/src/service/mouse_service.py
+++ python/autogame/src/service/mouse_service.py
@@ -10,23 +10,38 @@ import win32con
 
 class MouseService():
     @staticmethod
-    def mouse_click(hwnd, pos, down_type: str = 'left', times: int = 1):
+    def mouse_click(hwnd, pos1, pos2: tuple = None, down_type: str = 'left_down', times: int = 1, wait_time: int = 0.4):
         """
         鼠标点击操作
+        :param wait_time: 等待时间
         :param hwnd: 窗口句柄
-        :param pos: 坐标
-        :param down_type: 点击类型 left 左键 right 右键
+        :param pos1: 坐标1
+        :param pos2: 坐标2
+        :param down_type: 点击类型 left_down 左键点击 right_down 右键点击 left_move 左键拖拽 right_move 右键拖拽
         :param times: 点击次数 1次
         :return:
         """
-        x1 = pos[0]
-        y1 = pos[1]
-        param1 = win32api.MAKELONG(x1, y1)
-        # 左右键点击
-        if down_type == 'left':
-            win32api.PostMessage(hwnd, win32con.WM_LBUTTONDOWN, win32con.MK_LBUTTON, param1)
-        else:
-            win32api.PostMessage(hwnd, win32con.WM_LBUTTONDOWN, win32con.MK_LBUTTON, param1)
-        # 左右键释放
-        time.sleep(0.5)
-        win32api.PostMessage(hwnd, win32con.WM_LBUTTONUP, None, param1)
+        param1 = win32api.MAKELONG(pos1[0], pos1[1])
+        param2 = None
+        if pos2:
+            param2 = win32api.MAKELONG(pos2[0], pos1[2])
+        # 双击
+        for i_click in range(1, times):
+            # 左右键 点击
+            if down_type == 'left_down':
+                win32api.PostMessage(hwnd, win32con.WM_LBUTTONDOWN, win32con.MK_LBUTTON, param1)
+                time.sleep(wait_time)
+                win32api.PostMessage(hwnd, win32con.WM_LBUTTONUP, None, param1)
+            elif down_type == 'right_down':
+                win32api.PostMessage(hwnd, win32con.WM_RBUTTONDOWN, win32con.MK_LBUTTON, param1)
+                time.sleep(wait_time)
+                win32api.PostMessage(hwnd, win32con.WM_RBUTTONUP, None, param1)
+            # 左右键 拖拽
+            elif down_type == 'left_move':
+                win32api.PostMessage(hwnd, win32con.WM_LBUTTONDOWN, win32con.MK_LBUTTON, param1)
+                time.sleep(wait_time)
+                win32api.PostMessage(hwnd, win32con.WM_LBUTTONUP, None, param2)
+            elif down_type == 'right_move':
+                win32api.PostMessage(hwnd, win32con.WM_RBUTTONDOWN, win32con.MK_LBUTTON, param1)
+                time.sleep(wait_time)
+                win32api.PostMessage(hwnd, win32con.WM_RBUTTONUP, None, param2)
