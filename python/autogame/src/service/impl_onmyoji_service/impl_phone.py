b26d35f3e6a1d23f36adbba1076648ec1cac8e3b
orcakill <orcakill@dingtalk.com>
2024-04-16 18:10:26 +0800

    feat(python-autogame:项目结构调整): 项目结构调整
    项目结构调整

---
+++ python/autogame/src/service/impl_onmyoji_service/impl_phone.py
@@ -0,0 +1,20 @@
+# @Time: 2024年04月16日 14:48
+# @Author: orcakill
+# @File: impl_phone.py
+# @Description: 云手机自动登录连接
+
+class ImplPhone:
+
+    @staticmethod
+    def phone_login(game_device: str):
+        """
+        设备号
+        :param game_device:
+        :return:
+        """
+        # # 获取云手机句柄
+        # hwnd = ImageSerfind_hwnd(WinProcessName.phone_exe, WinClassName.phone_home)
+        # # 登录，或者根据设备号登录云手机
+        # ImageWindowsService.exists(hwnd, Onmyoji.phone_DL)
+        # 连接adb
+        # 同意连接
