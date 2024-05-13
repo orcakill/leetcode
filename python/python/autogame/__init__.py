9ec85f24f3d1530f61805587141f2171ec4a49b7
orcakill <orcakill@dingtalk.com>
2024-05-13 18:42:59 +0800

    feat(python-autogame:项目结构调整): 项目结构调整
    项目结构调整

---
+++ python/python/autogame/__init__.py
@@ -0,0 +1,4 @@
+# @Time: 2024年05月13日 16:51
+# @Author: orcakill
+# @File: __init__.py.py
+# @Description: TODO
--- python/python/other/temp/temp_word.py
+++ python/python/other/temp/temp_word.py
@@ -4,7 +4,8 @@
 # @Description: 将JHDM:"井号代码"转为 null JHDM /*井号代码*/,
 
 if __name__ == '__main__':
-    with open('D:\\1.txt', 'r', encoding='utf-8') as file:
+    with open('D:\\1.txt', 'r'
+            , encoding='utf-8') as file:
         for line in file:
             line = line.strip()  # 去除行首和行尾的空白字符
             if line:  # 判断是否为空行
