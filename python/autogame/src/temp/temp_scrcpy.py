3707e940ac6bace049a5036b8a4a5a5e915b4bb4
orcakill <orcakill@dingtalk.com>
2024-06-29 01:24:15 +0800

    feat(python-autogame:测试调整): python-autogame:测试调整
    python-autogame:测试调整

--- python/autogame/src/temp/temp_scrcpy.py
+++ python/autogame/src/temp/temp_scrcpy.py
@@ -1,7 +1,15 @@
+import datetime
+import os
 import subprocess
-import time, os, datetime
+import time
+
+import win32api
+import win32con
+import win32gui
+import win32ui
 from PIL import Image
-import win32gui, win32ui, win32api, win32con
+
+from src.service.impl_image_service.impl_hwnd import ImplHwnd
 from src.utils.my_logger import logger
 
 hwnd_title = dict()  # 获取windows窗口句柄+标题
@@ -53,16 +61,18 @@ def window_capture(filename, hwnd):  # 窗口截屏
     # 截取从左上角（0，0）长宽为（w，h）的图片
     saveDC.BitBlt((0, 0), (w2, h2), mfcDC, (0, 0), win32con.SRCCOPY)
     saveBitMap.SaveBitmapFile(saveDC, filename)
-    # 上面的截图尺寸不行，我就自己写了一个精确截图的def,其中x1,y1,x2,y2，诸君自行对比窗口截图
-    backpng = cut_pic_by_filename_XY(filename, 10, 45, 590, 1070)
-    return backpng
+    # # 上面的截图尺寸不行，我就自己写了一个精确截图的def,其中x1,y1,x2,y2，诸君自行对比窗口截图
+    # backpng = cut_pic_by_filename_XY(filename, 10, 45, 590, 1070)
+    # return backpng
+
 
 if __name__ == '__main__':
     # 可以运行以下注释的代码，获得手机的序列号,诸君可自行提取自己的手机序列号
     # cmdstr = "adb devices"
     # backstr = os.popen(cmdstr).read()
     # print(backstr)
-    device_name = "8ce78c9f"
+    # device_name = "8ce78c9f"
+    device_name = "A2CDUN4312H00817"
     device_n = ' -s ' + device_name
     # scrcpy --crop设定屏幕=1080*1920并且偏移坐标为 (0,0)
     # 这里的--crop命令就是老版本的-c命令，新版本升级了，-c命令是错误的了。
@@ -70,29 +80,21 @@ if __name__ == '__main__':
     # -m 1024 限制画面分辨率=1024
     # -b 4M修改视频码率=4M
     # --window-x 100 --window-y 200 --window-width 405 --window-height 720设定窗体起始坐标，设置窗体宽和高
-    win_title = "MY_Test1"
+    win_title = device_name
     # 注意这里是windows窗体标题，最好每次打开的windows窗体标题不同，否则多个窗体标题一样的话
     # 会出现错误，找不到句柄
     cmdstr = "scrcpy" + device_n + " --crop=1080:1920:0:0 --window-title "
     cmdstr = cmdstr + win_title
     cmdstr = cmdstr + " -m 1024 -b 4M"
-    logger.debug("执行命令{}",cmdstr)
-    subprocess.call(cmdstr, shell=True) # 打开scrcpy
-    time.sleep(3)  # 等待3秒，等待窗体彻底弹出
-    logger.debug("获取全部句柄")
-    win32gui.EnumWindows(_get_all_hwnd, 0)  # 获取所有windows窗口句柄
-    hwnd = 0
-    for wnd in hwnd_title.items():  # 循环获取所有的windows句柄
-        print(wnd)  # 句柄值示例：(325674,"MI 5")：前面是windows句柄ID，后面是windows窗口标题
-        get_win_title = str(wnd[1])
-        if win_title == get_win_title:  # 根据windows窗口标题判断，是不是需要截图的窗口。
-            hwnd = wnd[0]
-            break
+    logger.debug("执行命令{}", cmdstr)
+    subprocess.Popen(cmdstr, shell=True)  # 打开scrcpy
+    time.sleep(3)
+    logger.debug("根据标题获取句柄")
+    hwnd_info=ImplHwnd.get_all_hwnd_info(title=win_title)
+    hwnd = hwnd_info[0][3]
     logger.debug("windows句柄ID:", hwnd)
-    cut_image_file = './image/scrcpy' + str(device_name) + '.png'
+    cut_image_file = 'D://' + str(device_name) + '.png'
     get_png = window_capture(cut_image_file, hwnd)  # 对windows窗体进行截图
     logger.debug(get_png)
     # 关闭windows窗体
     # win32gui.PostMessage(hwnd, win32con.WM_CLOSE, 0, 0)
-
-
